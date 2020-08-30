120767dbcba49c9aa06625286dfcd3e9582aaf04
风之凌殇 <fzls.zju@gmail.com>
2020-08-31 04:27:20 +0800

    增加调用检查更新的逻辑

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from djc_helper import DjcHelper
+from update import check_update_on_start
 
 if __name__ == '__main__':
+    check_update_on_start()
+
     djcHelper = DjcHelper()
     djcHelper.run()

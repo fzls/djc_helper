35f30b959d202e2f06ae135fe32392b7bced8459
风之凌殇 <fzls.zju@gmail.com>
2020-09-03 21:43:45 +0800

    编译构建脚本增加使用log模块

--- _release.py
+++ _release.py
@@ -3,6 +3,7 @@ import re
 import shutil
 import subprocess
 from datetime import datetime
+from log import logger
 
 # ---------------准备工作
 version = input("请输入版本号：")
@@ -10,13 +11,13 @@ version = input("请输入版本号：")
 version_reg = r"\d+\.\d+\.\d+"
 
 if re.match(version_reg, version) is None:
-    print("版本号格式有误，正确的格式类似：1.0.0 ，而不是 {}".format(version))
+    logger.info("版本号格式有误，正确的格式类似：1.0.0 ，而不是 {}".format(version))
     exit(-1)
 
 version = 'v' + version
 
 run_start_time = datetime.now()
-print("开始发布版本 {}".format(version))
+logger.info("开始发布版本 {}".format(version))
 
 # 先声明一些需要用到的目录的地址
 dir_src = os.path.realpath('.')
@@ -52,15 +53,15 @@ if not os.path.isdir(dir_all_release):
 shutil.rmtree(dir_current_release, ignore_errors=True)
 os.mkdir(dir_current_release)
 # 复制文件与目录过去
-print("将以下内容从{}复制到{}".format(dir_src, dir_current_release))
+logger.info("将以下内容从{}复制到{}".format(dir_src, dir_current_release))
 for filename in files_to_copy:
     source = os.path.join(dir_src, filename)
     destination = os.path.join(dir_current_release, filename)
     if os.path.isdir(filename):
-        print("拷贝目录 {}".format(filename))
+        logger.info("拷贝目录 {}".format(filename))
         shutil.copytree(source, destination)
     else:
-        print("拷贝文件 {}".format(filename))
+        logger.info("拷贝文件 {}".format(filename))
         shutil.copyfile(source, destination)
 
 # ---------------构建
@@ -70,7 +71,7 @@ subprocess.call(["python", "_build.py"])
 
 # 压缩打包
 os.chdir(dir_all_release)
-print("开始压缩打包")
+logger.info("开始压缩打包")
 release_7z_name = '{}.7z'.format(release_dir_name)
 subprocess.call([path_bz, 'c', '-y', '-r', '-aoa', '-fmt:7z', '-l:9', release_7z_name, release_dir_name])
 
@@ -87,7 +88,7 @@ shutil.rmtree(release_without_chrome_dir_name)
 # ---------------推送版本到github
 # 打包完成后git添加标签
 os.chdir(dir_src)
-print("开始推送到github")
+logger.info("开始推送到github")
 # 先尝试移除该tag，并同步到github，避免后面加标签失败
 subprocess.call(['git', 'tag', '-d', version])
 subprocess.call(['git', 'push', 'origin', 'master', ':refs/tags/{version}'.format(version=version)])
@@ -96,8 +97,8 @@ subprocess.call(['git', 'tag', '-a', version, '-m', 'release {version}'.format(v
 subprocess.call(['git', 'push', 'origin', 'master', '--tags'])
 
 # ---------------结束
-print('+' * 40)
-print("发布完成，请将压缩包合适命名后上传至蓝奏云")
-print('+' * 40)
+logger.info('+' * 40)
+logger.info("发布完成，请将压缩包合适命名后上传至蓝奏云")
+logger.info('+' * 40)
 
 os.system("PAUSE")

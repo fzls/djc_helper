fc5ea215a1c2c087c6fd64716244067212b5bca4
风之凌殇 <fzls.zju@gmail.com>
2020-09-03 21:07:29 +0800

    调整日志格式，在命令行增加着色支持，使结果看上去更美观-。-

--- log.py
+++ log.py
@@ -4,10 +4,19 @@ import multiprocessing
 import pathlib
 import sys
 
+import colorlog
+
 ###########################################################
 #                         logging                         #
 ###########################################################
-logFormatter = logging.Formatter("%(asctime)s %(levelname)-5.5s [%(name)s] %(funcName)s:%(lineno)d: %(message)s")
+fileFmtStr = "%(asctime)s [%(name)s] %(funcName)s:%(lineno)d %(levelname)-5.5s: %(message)s"
+consoleFmtStr = "{}%(asctime)s {}[%(name)s] {}%(funcName)s:%(lineno)d {}%(levelname)-5.5s: %(message)s".format(
+    "%(purple)s",
+    "%(purple)s",
+    "%(purple)s",
+    "%(log_color)s",
+)
+
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 logger.name = "djc_helper"
@@ -23,11 +32,34 @@ process_name = multiprocessing.current_process().name
 if "MainProcess" in process_name:
     time_str = datetime.datetime.now().strftime('%Y_%m_%d_%H_%M_%S')
     fileHandler = logging.FileHandler("{0}/{1}_{2}_{3}.log".format(log_directory, logger.name, process_name, time_str), encoding="utf-8")
-    fileHandler.setFormatter(logFormatter)
+    fileLogFormatter = logging.Formatter(fileFmtStr)
+    fileHandler.setFormatter(fileLogFormatter)
     fileHandler.setLevel(logging.DEBUG)
     logger.addHandler(fileHandler)
 
+consoleLogFormatter = colorlog.ColoredFormatter(
+    consoleFmtStr,
+    datefmt=None,
+    reset=True,
+    log_colors={
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'red',
+    },
+    secondary_log_colors={},
+    style='%'
+)
 consoleHandler = logging.StreamHandler()
-consoleHandler.setFormatter(logFormatter)
+consoleHandler.setFormatter(consoleLogFormatter)
 consoleHandler.setLevel(logging.INFO)
 logger.addHandler(consoleHandler)
+
+if __name__ == '__main__':
+    consoleHandler.setLevel(logging.DEBUG)
+    logger.debug("debug")
+    logger.info("info")
+    logger.warning("warn")
+    logger.error("error")
+    logger.critical("critical")

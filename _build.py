d7a1798f476b17887e0832b480f9f7da434c414d
风之凌殇 <fzls.zju@gmail.com>
2020-09-03 22:05:06 +0800

    更新编译脚本，默认使用venv

--- _build.py
+++ _build.py
@@ -2,28 +2,54 @@
 import os
 import shutil
 import subprocess
+
 from log import logger
+
+logger.info("尝试初始化venv环境")
+subprocess.call([
+    "python",
+    "-m",
+    "venv",
+    ".venv",
+])
+
+pyscript_path = os.path.join(".venv", "Scripts")
+py_path = os.path.join(pyscript_path, "python")
+pip_path = os.path.join(pyscript_path, "pip")
+pyinstaller_path = os.path.join(pyscript_path, "pyinstaller")
+logger.info("使用.venv环境进行编译")
 
 exe_name = '道聚城助手.exe'
 icon = '道聚城.ico'
 
+logger.info("尝试更新pip")
+# python -m pip install --upgrade pip
+subprocess.call([
+    py_path,
+    "-m",
+    "pip",
+    "install",
+    "--upgrade",
+    "pip",
+])
+
 logger.info("尝试安装依赖库和pyinstaller")
 
 cmd_install_requiremnts = [
-    "pip",
+    pip_path,
     "install",
     "-i",
     "https://pypi.doubanio.com/simple",
     "-r",
     "requirements.txt",
-    "pyinstaller"
+    "pyinstaller",
 ]
 subprocess.call(cmd_install_requiremnts)
 
 logger.info("开始编译 {}".format(exe_name))
 
 cmd_build = [
-    'pyinstaller',
+    pyinstaller_path,
     '--icon', icon,
     '--name', exe_name,
     '-F',

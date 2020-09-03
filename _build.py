4a8647da07c7ee24ecb40c782ded973adf2eadff
风之凌殇 <fzls.zju@gmail.com>
2020-09-03 21:43:45 +0800

    编译构建脚本增加使用log模块

--- _build.py
+++ _build.py
@@ -2,11 +2,12 @@
 import os
 import shutil
 import subprocess
+from log import logger
 
 exe_name = '道聚城助手.exe'
 icon = '道聚城.ico'
 
-print("尝试安装依赖库和pyinstaller")
+logger.info("尝试安装依赖库和pyinstaller")
 
 cmd_install_requiremnts = [
     "pip",
@@ -19,7 +20,7 @@ cmd_install_requiremnts = [
 ]
 subprocess.call(cmd_install_requiremnts)
 
-print("开始编译 {}".format(exe_name))
+logger.info("开始编译 {}".format(exe_name))
 
 cmd_build = [
     'pyinstaller',
@@ -40,7 +41,7 @@ cmd_build = [
 
 subprocess.call(cmd_build)
 
-print("编译结束，进行善后操作")
+logger.info("编译结束，进行善后操作")
 # 复制二进制
 shutil.copyfile(os.path.join("dist", exe_name), exe_name)
 # 删除临时文件
@@ -49,4 +50,4 @@ for directory in ["build", "dist", "__pycache__"]:
 for file in ["道聚城助手.exe.spec"]:
     os.remove(file)
 
-print("done")
+logger.info("done")

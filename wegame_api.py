2cd6114714bbeb04261298ed51ff14e104e6ca9b
风之凌殇 <fzls.zju@gmail.com>
2020-09-15 00:20:29 +0800

    提交首版wegame接口

---
+++ wegame_api.py
@@ -0,0 +1,151 @@
+import requests
+from qq_login import QQLogin
+from config import config, load_config
+from util import uin2qq
+
+
+class WegameApi:
+    login_url = "https://www.wegame.com.cn/api/middle/clientapi/auth/login_by_qq"
+    common_url_prefix = "https://m.wegame.com.cn/api/mobile/lua/proxy/index/mwg_dnf_battle_record/"
+
+    def login(self, uin, skey):
+        self.set_uin_skey(skey, uin)
+
+        data = {
+            "login_info": {
+                "qq_info_type": 3,
+                "uin": uin2qq(self.uin),
+                "sig": self.skey,
+            },
+            "config_params": {
+                "lang_type": 0
+            },
+            "mappid": "10001",
+            "mcode": "",
+            "clienttype": "1000005"
+        }
+        headers = {
+            "referer": "https://www.wegame.com.cn/middle/login/third_callback.html",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
+        }
+        res = requests.post(self.login_url, json=data, headers=headers)
+        tgp_id, tgp_ticket = int(res.cookies.get('tgp_id')), res.cookies.get('tgp_ticket')
+        self.set_tgp_info(tgp_id, tgp_ticket)
+        print(tgp_id)
+        print(tgp_ticket)
+
+    def set_uin_skey(self, skey, uin):
+        self.uin = uin
+        self.skey = skey
+
+    def set_tgp_info(self, tgp_id, tgp_ticket):
+        self.tgp_id = tgp_id
+        self.tgp_ticket = tgp_ticket
+        # 需自行调用set_role_info设置
+        self.area_id = 0
+        self.role_name = ""
+        self.common_headers = {
+            "accept": "application/json",
+            "cookie": "app_id=10001;tgp_id={tgp_id};platform=qq;account={account};skey={skey};tgp_ticket={tgp_ticket};machine_type=MIX+2;channel_number=5;app_version=1050602003;client_type=601".format(
+                tgp_id=self.tgp_id,
+                account=uin2qq(self.uin),
+                skey=self.skey,
+                tgp_ticket=self.tgp_ticket,
+            ),
+            "Content-Type": "application/json; charset=utf-8",
+            "Connection": "Keep-Alive",
+            "Accept-Encoding": "gzip",
+            "User-Agent": "okhttp/3.11.0",
+        }
+
+    def get_player_role_list(self):
+        # 获取玩家所有区服的的角色列表
+        return self._post("get_player_role_list", need_role_info=False).json()
+
+    def set_role_info(self, area_id, role_name):
+        # 调用下列接口前需要先调用该接口设置角色信息
+        self.area_id = int(area_id)
+        self.role_name = str(role_name)
+
+    def get_capacity_detail_info(self):
+        # 获取指定服务器的指定角色的战力信息
+        return self._post("get_capacity_detail_info").json()
+
+    def get_player_fight_statistic_info(self):
+        # 获取指定服务器的指定角色的面板一览
+        return self._post("get_player_fight_statistic_info").json()
+
+    def get_equip_description_image(self, equip_id):
+        # 获取指定装备的描述图片
+        return "https://bb.img.qq.com/bbcdn/dnf/equips/equimg/{equip_id}.png".format(equip_id=equip_id)
+
+    def get_equip_icon(self, equip_id):
+        # 获取指定装备的图标
+        return "http://cdn.tgp.qq.com/DNF_picture/equip_icon/{equip_id}.png".format(equip_id=equip_id)
+
+    def get_player_equipment_list(self):
+        # 获取指定服务器的指定角色的面板一览
+        return self._post("get_player_equipment_list").json()
+
+    def get_player_role_detail(self):
+        # 获取指定服务器的指定角色的详细面板数据
+        return self._post("get_player_role_detail").json()
+
+    def get_player_role_info(self):
+        # 获取指定服务器的指定角色的角色信息
+        return self._post("get_player_role_info").json()
+
+    def get_player_recent_dungeon_list(self, career):
+        # 获取指定服务器的指定角色的最近副本伤害信息，职业ID可从get_player_role_info接口中获取
+        return self._post("get_player_recent_dungeon_list", json_data={
+            "start_index":0,
+            "career": career,
+        }).json()
+
+    def _post(self, api_name, json_data=None, need_role_info=True):
+        if need_role_info and len(self.role_name) == 0:
+            print("调用除查询角色列表外任意接口前请先调用set_role_info设置角色信息，若不知道角色信息，可以调用get_player_role_list获取角色信息")
+            exit(-1)
+
+        base_json_data = {
+            "target_tgpid": self.tgp_id,
+            # "target_suid": "0",
+            "area_id": self.area_id,
+            "role": self.role_name,
+            "role_name": self.role_name,
+        }
+        if json_data is None:
+            json_data = {}
+        res = requests.post(self.common_url_prefix + api_name, json={**base_json_data, **json_data}, headers=self.common_headers)
+
+        # 调试用
+        import json
+        print(json.dumps(res.json(), ensure_ascii=False))
+
+        return res
+
+
+if __name__ == '__main__':
+    api = WegameApi()
+
+    # # 读取配置信息
+    load_config("config.toml", "config.toml.local")
+    cfg = config()
+
+    ql = QQLogin(cfg.common)
+    lr = ql.login(cfg.account_configs[0].account_info.account, cfg.account_configs[0].account_info.password)
+
+    print(lr)
+    api.login(lr.uin, lr.skey)
+    res = api.get_player_role_list()
+    for role in res['data']['role_list']:
+        print(role['area_id'], role['role_name'])
+    api.set_role_info(res['data']['role_list'][0]['area_id'], res['data']['role_list'][0]['role_name'])
+    api.get_capacity_detail_info()
+    api.get_player_fight_statistic_info()
+    api.get_equip_description_image(100390332)
+    api.get_equip_icon(100390332)
+    api.get_player_equipment_list()
+    api.get_player_role_detail()
+    api.get_player_role_info()
+    api.get_player_recent_dungeon_list(3)

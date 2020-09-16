37fd9f42a442ade6aadbf607f3fa6a493043e790
风之凌殇 <fzls.zju@gmail.com>
2020-09-16 15:36:41 +0800

    启动时默认最大化窗口

--- util.py
+++ util.py
@@ -1,5 +1,14 @@
+import win32con
+import win32gui
+
+
 def uin2qq(uin):
     return str(uin)[1:].lstrip('0')
+
+
+def maximize_console():
+    hwnd = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(hwnd, win32con.SW_MAXIMIZE)
 
 
 if __name__ == '__main__':

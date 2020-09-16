3eb886d65268ac6fdfd18ea6a970f71f531c7cf5
风之凌殇 <fzls.zju@gmail.com>
2020-09-16 21:31:56 +0800

    将全部url移到单独的类中，方便后续管理

--- djc_helper.py
+++ djc_helper.py
@@ -12,6 +12,7 @@ from game_info import get_game_info, get_game_info_by_bizcode
 from network import *
 from qq_login import QQLogin
 from sign import getMillSecondsUnix
+from urls import Urls
 
 
 # DNF蚊子腿小助手
@@ -34,59 +35,8 @@ class DjcHelper:
         # 初始化网络相关设置
         self.init_network()
 
-        # 余额
-        self.balance = "https://djcapp.game.qq.com/cgi-bin/daoju/djcapp/v5/solo/jfcloud_flow.cgi?&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&&method=balance&page=0&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        self.money_flow = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.bean.water&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&page=1&starttime={starttime}&endtime={endtime}&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 每日登录事件：imsdk登录
-        self.imsdk_login = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.message.imsdk.login&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 每日登录事件：app登录
-        self.user_login_event = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.login.user.first&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 每日签到的奖励规则
-        self.sign_reward_rule = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.reward.sign.rule&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&output_format=json&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 签到相关接口的入口
-        self.sign = "https://comm.ams.game.qq.com/ams/ame/amesvr?ameVersion=0.3&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sServiceType=dj&iActivityId=11117&sServiceDepartment=djc&set_info=newterminals&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&&appSource=android&ch=10003&osVersion=Android-28&sVersionName=v4.1.6.0"
-        self.sign_raw_data = "appVersion={appVersion}&g_tk={g_tk}&iFlowId={iFlowId}&month={month}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&sign_version=1.0&ch=10003&iActivityId=11117&osVersion=Android-28&sVersionName=v4.1.6.0&sServiceDepartment=djc&sServiceType=dj&appSource=android"
-
-        # 心悦相关接口的入口
-        self.xinyue_iActivityId = "166962"
-        self.xinyue = "https://act.game.qq.com/ams/ame/amesvr?ameVersion=0.3&sSDID={sSDID}&sMiloTag={sMiloTag}&sServiceType=tgclub&iActivityId={xinyue_iActivityId}&sServiceDepartment=xinyue&isXhrPost=true"
-        self.xinyue_raw_data = "iActivityId={xinyue_iActivityId}&g_tk={g_tk}&iFlowId={iFlowId}&package_id={package_id}&xhrPostKey=xhr_{millseconds}&eas_refer=http%3A%2F%2Fnoreferrer%2F%3Freqid%3D{uuid}%26version%3D23&lqlevel={lqlevel}&teamid={teamid}&e_code=0&g_code=0&eas_url=http%3A%2F%2Fxinyue.qq.com%2Fact%2Fa20181101rights%2F&xhr=1&sServiceDepartment=xinyue&sServiceType=tgclub"
-
-        # 每月黑钻等级礼包
-        self.heizuan_gift = "https://dnf.game.qq.com/mtask/lottery/?r={rand}&serviceType=dnf&channelId=1&actIdList=44c24e"
-
-        # 信用星级礼包
-        self.credit_gift = "https://dnf.game.qq.com/mtask/lottery/?r={rand}&serviceType=dnf&channelId=1&actIdList=13c48b"
-
-        # 腾讯游戏信用
-        self.credit_xinyue_gift = "https://gamecredit.qq.com/api/qq/proxy/credit_xinyue_gift?gift_group={gift_group}"
-
-        # 任务列表
-        self.usertask = "https://djcapp.game.qq.com/daoju/v3/api/we/usertaskv2/Usertask.php?iAppId=1001&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&_app_id=1001&output_format=json&_output_fmt=json&appid=1001&optype=get_usertask_list&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 领取任务奖励
-        self.take_task_reward = "https://djcapp.game.qq.com/daoju/v3/api/we/usertaskv2/Usertask.php?iAppId=1001&appVersion={appVersion}&iruleId={iruleId}&p_tk={p_tk}&sDeviceID={sDeviceID}&_app_id=1001&output_format=json&_output_fmt=json&appid=1001&optype=receive_usertask&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 上报任务完成
-        self.task_report = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.task.report&appVersion={appVersion}&task_type={task_type}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 查询道聚城绑定的各游戏角色列表，dnf的角色信息和选定手游的角色信息将从这里获取
-        self.query_bind_role_list = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.role.bind_list&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&type=1&output_format=json&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 具体游戏参数可查阅djc_biz_list.json
-        self.query_game_server_list = "https://gameact.qq.com/comm-htdocs/js/game_area/utf8verson/{bizcode}_server_select_utf8.js"
-        # 查询手游礼包礼包，不同手游传入不同bizcode
-        self.query_game_gift_bags = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.package.list&bizcode={bizcode}&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&output_format=json&optype=get_user_package_list&appid=1001&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&showType=qq&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 查询手游角色列表，不同手游传入不同game(gameCode), sAMSTargetAppId(wxAppid)
-        self.get_game_role_list = "https://comm.aci.game.qq.com/main?sCloudApiName=ams.gameattr.role&game={game}&sAMSTargetAppId={sAMSTargetAppId}&appVersion={appVersion}&area={area}&platid={platid}&partition={partition}&callback={callback}&p_tk={p_tk}&sDeviceID={sDeviceID}&&sAMSAcctype=pt&&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 一键领取手游礼包，不同手游传入不同bizcode
-        self.recieve_game_gift = "https://djcapp.game.qq.com/daoju/igw/main/?_service=app.package.receive&bizcode={bizcode}&appVersion={appVersion}&iruleId={iruleId}&sPartition={sPartition}&roleCode={roleCode}&sRoleName={sRoleName}&channelID={channelID}&channelKey={channelKey}&systemID={systemID}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&&weexVersion=0.9.4&platform=android&deviceModel=MIX%202&appid=1001&output_format=json&optype=receive_usertask_game&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-
-        # 兑换道具--requestConvertCoupon
-        self.exchangeItems = "https://apps.game.qq.com/cgi-bin/daoju/v3/hs/i_buy.cgi?&weexVersion=0.9.4&appVersion={appVersion}&iGoodsSeqId={iGoodsSeqId}&iZone={iZone}&lRoleId={lRoleId}&rolename={rolename}&p_tk={p_tk}&sDeviceID={sDeviceID}&sDjcSign={sDjcSign}&platform=android&deviceModel=MIX%202&&&_output_fmt=1&_plug_id=9800&_from=app&iActionId=2594&iActionType=26&_biz_code=dnf&biz=dnf&appid=1003&_app_id=1003&_cs=2&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
-        # 获取所有可兑换的道具的列表
-        self.show_exchange_item_list = "https://app.daoju.qq.com/jd/js/dnf_index_list_dj_info_json.js?&weexVersion=0.9.4&appVersion={appVersion}&p_tk={p_tk}&sDeviceID={sDeviceID}&platform=android&deviceModel=MIX%202&&osVersion=Android-28&ch=10003&sVersionName=v4.1.6.0&appSource=android"
+        # 相关链接
+        self.urls = Urls()
 
     def init_network(self):
         self.network = Network(self.cfg.sDeviceID, self.cfg.account_info.uin, self.cfg.account_info.skey, self.common_cfg)
@@ -330,14 +280,14 @@ class DjcHelper:
         self.init_network()
 
     def query_balance(self, ctx, print_res=True):
-        return self.get(ctx, self.balance, print_res=print_res)
+        return self.get(ctx, self.urls.balance, print_res=print_res)
 
     def query_money_flow(self, ctx):
-        return self.get(ctx, self.money_flow)
+        return self.get(ctx, self.urls.money_flow)
 
     def get_bind_role_list(self):
         # 查询全部绑定角色信息
-        res = self.get("获取道聚城各游戏的绑定角色列表", self.query_bind_role_list, print_res=False)
+        res = self.get("获取道聚城各游戏的绑定角色列表", self.urls.query_bind_role_list, print_res=False)
         self.bizcode_2_bind_role_map = {}
         for roleinfo_dict in res["data"]:
             role_info = GameRoleInfo()
@@ -358,29 +308,29 @@ class DjcHelper:
 
     def sign_in_and_take_awards(self):
         # 发送登录事件，否则无法领取签到赠送的聚豆，报：对不起，请在掌上道聚城app内进行签到
-        self.get("2.1.1 发送imsdk登录事件", self.imsdk_login)
-        self.get("2.1.2 发送app登录事件", self.user_login_event)
+        self.get("2.1.1 发送imsdk登录事件", self.urls.imsdk_login)
+        self.get("2.1.2 发送app登录事件", self.urls.user_login_event)
         # 签到
-        self.post("2.2 签到", self.sign, self.sign_flow_data("96939"))
+        self.post("2.2 签到", self.urls.sign, self.sign_flow_data("96939"))
         # 领取本日签到赠送的聚豆
-        self.post("2.3 领取签到赠送的聚豆", self.sign, self.sign_flow_data("324410"))
+        self.post("2.3 领取签到赠送的聚豆", self.urls.sign, self.sign_flow_data("324410"))
 
         # 尝试领取自动签到的奖励
         # 查询本月签到的日期列表
-        signinDates = self.post("2.3.1 查询签到日期列表", self.sign, self.sign_flow_data("96938"), print_res=False)
+        signinDates = self.post("2.3.1 查询签到日期列表", self.urls.sign, self.sign_flow_data("96938"), print_res=False)
         month_total_signed_days = len(signinDates["modRet"]["data"])
         # 根据本月已签到数，领取符合条件的每月签到若干日的奖励（也就是聚豆页面最上面的那个横条）
-        for sign_reward_rule in self.get("2.3.2 查询连续签到奖励规则", self.sign_reward_rule, print_res=False)["data"]:
+        for sign_reward_rule in self.get("2.3.2 查询连续签到奖励规则", self.urls.sign_reward_rule, print_res=False)["data"]:
             if sign_reward_rule["iCanUse"] == 1 and month_total_signed_days >= int(sign_reward_rule["iDays"]):
                 ctx = "2.3.3 领取连续签到{}天奖励".format(sign_reward_rule["iDays"])
-                self.post(ctx, self.sign, self.sign_flow_data(str(sign_reward_rule["iFlowId"])))
+                self.post(ctx, self.urls.sign, self.sign_flow_data(str(sign_reward_rule["iFlowId"])))
 
     def sign_flow_data(self, iFlowId):
-        return self.format(self.sign_raw_data, iFlowId=iFlowId)
+        return self.format(self.urls.sign_raw_data, iFlowId=iFlowId)
 
     def complete_tasks(self):
         # 完成《绝不错亿》
-        self.get("3.1 模拟点开活动中心", self.task_report, task_type="activity_center")
+        self.get("3.1 模拟点开活动中心", self.urls.task_report, task_type="activity_center")
 
         # 完成《礼包达人》
         cfg = self.cfg.mobile_game_role_info
@@ -390,7 +340,7 @@ class DjcHelper:
             giftInfos = self.get_mobile_game_gifts()
             dayIndex = datetime.datetime.now().weekday()  # 0-周一...6-周日，恰好跟下标对应
             giftInfo = giftInfos[dayIndex]
-            self.get("3.2 一键领取{}日常礼包-{}".format(cfg.game_name, giftInfo.sTask), self.recieve_game_gift,
+            self.get("3.2 一键领取{}日常礼包-{}".format(cfg.game_name, giftInfo.sTask), self.urls.recieve_game_gift,
                      bizcode=game_info.bizCode, iruleId=giftInfo.iRuleId,
                      systemID=role_info.systemID, sPartition=role_info.areaID, channelID=role_info.channelID, channelKey=role_info.channelKey,
                      roleCode=role_info.roleCode, sRoleName=role_info.roleName)
@@ -416,11 +366,11 @@ class DjcHelper:
 
     def take_task_award(self, prefix, iRuleId, taskName=""):
         ctx = "{} 查询当前任务状态".format(prefix)
-        taskinfo = self.get(ctx, self.usertask, print_res=False)
+        taskinfo = self.get(ctx, self.urls.usertask, print_res=False)
 
         if self.can_take_task_award(taskinfo, iRuleId):
             ctx = "{} 领取任务-{}-奖励".format(prefix, taskName)
-            self.get(ctx, self.take_task_reward, iruleId=iRuleId)
+            self.get(ctx, self.urls.take_task_reward, iruleId=iRuleId)
 
     # 尝试领取每日任务奖励
     def can_take_task_award(self, taskinfo, iRuleId):
@@ -459,7 +409,7 @@ class DjcHelper:
 
     def exchange_item(self, ctx, iGoodsSeqId):
         roleinfo = self.bizcode_2_bind_role_map["dnf"].sRoleInfo
-        return self.get(ctx, self.exchangeItems, iGoodsSeqId=iGoodsSeqId, rolename=roleinfo.roleName, lRoleId=roleinfo.roleCode, iZone=roleinfo.serviceID)
+        return self.get(ctx, self.urls.exchangeItems, iGoodsSeqId=iGoodsSeqId, rolename=roleinfo.roleName, lRoleId=roleinfo.roleCode, iZone=roleinfo.serviceID)
 
     def query_all_extra_info(self):
         # 获取玩家的dnf角色列表，note:当不知道角色的roleid和rolename的时候，可以取消注释进行查询
@@ -474,7 +424,7 @@ class DjcHelper:
         ctx = "获取账号({})的dnf角色列表".format(self.cfg.name)
         game_info = get_game_info("地下城与勇士")
         roleinfo = self.bizcode_2_bind_role_map["dnf"].sRoleInfo
-        roleListJsonRes = self.get(ctx, self.get_game_role_list, game=game_info.gameCode, sAMSTargetAppId=game_info.wxAppid, area=roleinfo.serviceID, platid="", partition="", is_jsonp=True, print_res=False)
+        roleListJsonRes = self.get(ctx, self.urls.get_game_role_list, game=game_info.gameCode, sAMSTargetAppId=game_info.wxAppid, area=roleinfo.serviceID, platid="", partition="", is_jsonp=True, print_res=False)
         roleLists = json_parser.parse_role_list(roleListJsonRes)
         lines = []
         lines.append("")
@@ -502,7 +452,7 @@ class DjcHelper:
             logger.info("未启用自动完成《礼包达人》任务功能")
             return
 
-        roleListJsonRes = self.get(ctx, self.get_game_role_list, game=game_info.gameCode, sAMSTargetAppId=game_info.wxAppid, area=cfg.area, platid=cfg.platid, partition=cfg.partition, is_jsonp=True, print_res=False)
+        roleListJsonRes = self.get(ctx, self.urls.get_game_role_list, game=game_info.gameCode, sAMSTargetAppId=game_info.wxAppid, area=cfg.area, platid=cfg.platid, partition=cfg.partition, is_jsonp=True, print_res=False)
         roleList = json_parser.parse_mobile_game_role_list(roleListJsonRes)
         lines = []
         lines.append("")
@@ -523,18 +473,18 @@ class DjcHelper:
 
     def open_mobile_game_server_list(self):
         game_info = self.get_mobile_game_info()
-        res = requests.get(self.query_game_server_list.format(bizcode=game_info.bizCode))
+        res = requests.get(self.urls.query_game_server_list.format(bizcode=game_info.bizCode))
         server_list_file = "reference_data/server_list_{bizName}.js".format(bizName=game_info.bizName)
         with open(server_list_file, 'w', encoding='utf-8') as f:
             f.write(res.text)
         os.popen("notepad.exe {}".format(server_list_file))
 
     def query_dnf_gifts(self):
-        self.get("查询可兑换道具列表", self.show_exchange_item_list)
+        self.get("查询可兑换道具列表", self.urls.show_exchange_item_list)
 
     def get_mobile_game_gifts(self):
         game_info = self.get_mobile_game_info()
-        data = self.get("查询{}礼包信息".format(game_info), self.query_game_gift_bags, bizcode=game_info.bizCode, print_res=False)
+        data = self.get("查询{}礼包信息".format(game_info), self.urls.query_game_gift_bags, bizcode=game_info.bizCode, print_res=False)
 
         sign_in_gifts = []
         for raw_gift in data["data"]["list"]["data"]:
@@ -773,16 +723,16 @@ class DjcHelper:
         return XinYueInfo(score, ysb, xytype, specialMember, username, usericon)
 
     def xinyue_op(self, ctx, iFlowId, package_id="", print_res=True, lqlevel=1, teamid=""):
-        return self.post(ctx, self.xinyue, self.xinyue_flow_data(iFlowId, package_id, lqlevel, teamid), sMiloTag=self.make_s_milo_tag(iFlowId))
+        return self.post(ctx, self.urls.xinyue, self.xinyue_flow_data(iFlowId, package_id, lqlevel, teamid), sMiloTag=self.make_s_milo_tag(iFlowId))
 
     def xinyue_flow_data(self, iFlowId, package_id="", lqlevel=1, teamid=""):
         # 网站上特邀会员不论是游戏家G几，调用doAction(flowId,level)时level一律传1，而心悦会员则传入实际的567对应心悦123
         if lqlevel < 5:
             lqlevel = 1
-        return self.format(self.xinyue_raw_data, iFlowId=iFlowId, package_id=package_id, lqlevel=lqlevel, teamid=teamid)
+        return self.format(self.urls.xinyue_raw_data, iFlowId=iFlowId, package_id=package_id, lqlevel=lqlevel, teamid=teamid)
 
     def make_s_milo_tag(self, iFlowId):
-        iActivityId, id = self.xinyue_iActivityId, self.cfg.account_info.uin
+        iActivityId, id = self.urls.xinyue_iActivityId, self.cfg.account_info.uin
         return "AMS-MILO-{iActivityId}-{iFlowId}-{id}-{millseconds}-{rand6}".format(
             iActivityId=iActivityId,
             iFlowId=iFlowId,
@@ -799,7 +749,7 @@ class DjcHelper:
             logger.warning("未启用领取每月黑钻等级礼包功能，将跳过")
             return
 
-        res = self.get("领取每月黑钻等级礼包", self.heizuan_gift)
+        res = self.get("领取每月黑钻等级礼包", self.urls.heizuan_gift)
         # 如果未绑定大区，提示前往绑定 "iRet": -50014, "sMsg": "抱歉，请先绑定大区后再试！"
         if res["iRet"] == -50014:
             msg = "领取每月黑钻等级礼包失败，请先前往黑钻页面绑定角色信息\n若无需该功能，可前往配置文件自行关闭该功能"
@@ -808,9 +758,9 @@ class DjcHelper:
         return res
 
     def get_credit_xinyue_gift(self):
-        self.get("每月信用星级礼包", self.credit_gift)
-        self.get("腾讯游戏信用-高信用即享礼包", self.credit_xinyue_gift, gift_group=1)
-        self.get("腾讯游戏信用-高信用&游戏家即享礼包", self.credit_xinyue_gift, gift_group=2)
+        self.get("每月信用星级礼包", self.urls.credit_gift)
+        self.get("腾讯游戏信用-高信用即享礼包", self.urls.credit_xinyue_gift, gift_group=1)
+        self.get("腾讯游戏信用-高信用&游戏家即享礼包", self.urls.credit_xinyue_gift, gift_group=2)
 
     # --------------------------------------------辅助函数--------------------------------------------
     def get(self, ctx, url, pretty=False, print_res=True, is_jsonp=False, **params):
@@ -832,7 +782,7 @@ class DjcHelper:
             "month": self.get_month(),
             "starttime": self.getMoneyFlowTime(startTime.year, startTime.month, startTime.day, startTime.hour, startTime.minute, startTime.second),
             "endtime": self.getMoneyFlowTime(endTime.year, endTime.month, endTime.day, endTime.hour, endTime.minute, endTime.second),
-            "xinyue_iActivityId": self.xinyue_iActivityId,
+            "xinyue_iActivityId": self.urls.xinyue_iActivityId,
             "sSDID": self.cfg.sDeviceID.replace('-', ''),
             "uuid": self.cfg.sDeviceID,
             "millseconds": getMillSecondsUnix(),

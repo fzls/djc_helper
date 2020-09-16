fc347efa8bb1d32ce87f01d0493de499d39bc3a6
风之凌殇 <fzls.zju@gmail.com>
2020-09-16 20:30:55 +0800

    调整角色信息的获取方式，改为直接使用从道聚城获取的绑定角色列表信息，由玩家自行在道聚城设置

--- game_info.py
+++ game_info.py
@@ -6,22 +6,27 @@ import win32con
 
 from dao import GameInfo
 
-all_game_info_map = {}
+name_2_game_info_map = {}
+code_2_game_info_map = {}
 
 with open("reference_data/djc_biz_list.json", "r", encoding="utf-8") as f:
     raw_data = json.load(f)
     for game_data in raw_data["data"]:
-        all_game_info_map[game_data["bizName"]] = GameInfo(game_data)
+        gameInfo = GameInfo(game_data)
+        name_2_game_info_map[game_data["bizName"]] = GameInfo(game_data)
+        code_2_game_info_map[game_data["bizCode"]] = GameInfo(game_data)
 
 
 def get_game_info(name):
-    if name not in all_game_info_map:
+    if name not in name_2_game_info_map:
         win32api.MessageBox(0, "未找到游戏【{}】相关的配置，可能是空格等不完全匹配，请在稍后打开的文件中查找对应游戏的实际名字".format(name), "游戏名不正确", win32con.MB_ICONWARNING)
         subprocess.call(["notepad.exe", "reference_data/djc_biz_list.json"])
         exit(-1)
 
-    return all_game_info_map[name]
+    return name_2_game_info_map[name]
 
+def get_game_info_by_bizcode(bizcode):
+    return code_2_game_info_map[bizcode]
 
 if __name__ == '__main__':
     print(get_game_info("剑网3:指尖江湖"))

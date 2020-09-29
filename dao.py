b3829883cae2e4293d14d76b1861b51b657eb40f
风之凌殇 <fzls.zju@gmail.com>
2020-09-29 18:41:05 +0800

    增加支持道聚城许愿

--- dao.py
+++ dao.py
@@ -49,6 +49,90 @@ class RoleInfo(ConfigInterface):
         self.type = "1"
 
 
+class GoodsInfo(ConfigInterface):
+    def __init__(self):
+        self.type = "3"
+        self.actId = "3"
+        self.propId = "24074"
+        self.propName = "曜-云鹰飞将"
+        self.busId = "yxzj"
+        self.propDesc = ""
+        self.propImg = "http://ossweb-img.qq.com/images/daoju/app/yxzj/rectangle/13-52202-0-r684.jpg?_t=1596187484"
+        self.propImg2 = ""
+        self.propVideoId = ""
+        self.propCoverId = ""
+        self.limitPerOrder = "1"
+        self.totalLimit = "0"
+        self.recommend = "80"
+        self.valiDate = []  # type: List[GoodsValiDateInfo]
+        self.heroSkin = []
+        self.related = False
+        self.category = GoodsCategoryInfo()
+        self.isCombinPkg = 0
+        self.IsOwn = 0
+
+    def auto_update_config(self, raw_config: dict):
+        super().auto_update_config(raw_config)
+
+        if 'valiDate' in raw_config:
+            self.valiDate = []
+            for cfg in raw_config["valiDate"]:
+                ei = GoodsValiDateInfo()
+                ei.auto_update_config(cfg)
+                self.valiDate.append(ei)
+
+
+class GoodsValiDateInfo(ConfigInterface):
+    def __init__(self):
+        self.day = "永久"
+        self.name = "曜-云鹰飞将"
+        self.pic = "http://ossweb-img.qq.com/images/daoju/app/yxzj/rectangle/13-52202-0-r684.jpg?_t=1596187484"
+        self.picMid = None
+        self.code = "24074"
+        self.gameCode = "52202"
+        self.sendType = 13
+        self.oldPrice = "16880"
+        self.curPrice = "16880"
+        self.iGoldPrice = "0"
+        self.iDqPrice = "16880"
+        self.rushBegin = "0000-00-00 00:00:00"
+        self.rushEnd = "0000-00-00 00:00:00"
+        self.isMobile = 0
+        self.waterMark = 0
+        self.label = ""
+        self.rushPrice = 0
+        self.gold_price_rush = 0
+        self.dq_price_rush = 0
+        self.twin_price = 0
+        self.twin_dq_price = 0
+        self.twin_code = ""
+        self.isskin = 0
+        self.left = "0"
+        self.bought = "0"
+        self.todayBought = 0
+        self.award = {"list"  : []}
+        self.isFunc = 0
+        self.beanCut = 0
+        self.maxBeanCutPrice = 0
+        self.maxBeanCutNum = 0
+        self.beanRush = 0
+        self.beanBegin = "0000-00-00 00:00:00"
+        self.beanEnd = "0000-00-00 00:00:00"
+        self.acctPlat = "0"
+        self.supportPresent = "1"
+        self.supportCart = "1"
+        self.pinPrice = 0
+        self.pinDqPrice = 0
+        self.pinBegin = "0000-00-00 00:00:00"
+        self.pinEnd = "0000-00-00 00:00:00"
+
+
+class GoodsCategoryInfo(ConfigInterface):
+    def __init__(self):
+        self.mainCategory = "170"
+        self.subCategory = "0"
+
+
 class DnfRoleInfo(DaoObject):
     def __init__(self, roleid, rolename, forceid, level):
         self.roleid = int(roleid)

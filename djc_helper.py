223d3617b667ec41f5ef151c6bf935eb75484058
风之凌殇 <fzls.zju@gmail.com>
2020-09-30 08:20:36 +0800

    加一下兼容

--- djc_helper.py
+++ djc_helper.py
@@ -448,8 +448,8 @@ class DjcHelper:
 
         # 查询许愿道具信息
         query_wish_item_list_res = self.get("3.3.0  查询许愿道具", self.urls.query_wish_goods_list, plat=roleModel.systemID, biz=roleModel.bizCode, print_res=False)
-        if len(query_wish_item_list_res["data"]) == 0:
-            logger.warning("游戏【{}】暂不支持许愿".format(roleModel.gameName))
+        if "data" not in query_wish_item_list_res or len(query_wish_item_list_res["data"]) == 0:
+            logger.warning("游戏【{}】暂不支持许愿, query_wish_item_list_res={}".format(roleModel.gameName, query_wish_item_list_res))
             return
 
         propModel = GoodsInfo()

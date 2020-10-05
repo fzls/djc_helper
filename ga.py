0c378eea88428c0124d7606519b577bf45d3b95f
风之凌殇 <fzls.zju@gmail.com>
2020-10-05 22:07:47 +0800

    增加pageview

--- ga.py
+++ ga.py
@@ -14,14 +14,14 @@ def track_event(category, action, label=None, value=0):
         data = {
             'v': '1',  # API Version.
             'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
-            # Anonymous Client Identifier. Ideally, this should be a UUID that is associated with particular user, device, or browser instance.
-            'cid': get_cid(),
+            'cid': get_cid(),  # Anonymous Client Identifier. Ideally, this should be a UUID that is associated with particular user, device, or browser instance.
+            'ua': 'requests',
+
             't': 'event',  # Event hit type.
             'ec': category,  # Event category.
             'ea': action,  # Event action.
             'el': label,  # Event label.
             'ev': value,  # Event value, must be an integer
-            'ua': 'requests'
         }
 
         requests.post('https://www.google-analytics.com/collect', data=data, timeout=10)
@@ -30,9 +30,31 @@ def track_event(category, action, label=None, value=0):
         logger.exception(msg, exc_info=exc)
 
 
+def track_page(page):
+    try:
+        title = ''.split('/')[-1]
+        data = {
+            'v': '1',  # API Version.
+            'tid': GA_TRACKING_ID,  # Tracking ID / Property ID.
+            'cid': get_cid(),  # Anonymous Client Identifier. Ideally, this should be a UUID that is associated with particular user, device, or browser instance.
+            'ua': 'requests',
+
+            't': 'pageview',  # Event hit type.
+            'dh': "app.djc-helper",  # Document hostname.
+            'dp': page,  # Page.
+            'dt': title,  # Title.
+        }
+
+        requests.post('https://www.google-analytics.com/collect', data=data, timeout=10)
+    except Exception as exc:
+        msg = "track_page failed, page={}".format(page)
+        logger.exception(msg, exc_info=exc)
+
+
 def get_cid():
     return "{}-{}".format(platform.node(), uuid.getnode())
 
 
 if __name__ == '__main__':
-    track_event("example", "test")
+    # track_event("example", "test")
+    track_page("/example/test")

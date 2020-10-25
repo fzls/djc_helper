c0e6584982cafe4bc02551209ac570f72442d545
风之凌殇 <fzls.zju@gmail.com>
2020-10-26 01:42:35 +0800

    运行结束展示一下小助手的整体使用情况

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from ark_lottery import ArkLottery
 from config import load_config, config, XinYueOperationConfig
 from djc_helper import DjcHelper
 from update import check_update_on_start
-from usage_count import increase_counter, get_count, get_uuid
+from usage_count import *
 from util import *
 from version import *
+
+global_usage_counter_name = "global_count"
+user_usage_counter_name_prefix = "user_count"
+my_usage_counter_name = "{}/{}".format(user_usage_counter_name_prefix, get_uuid())
 
 
 def has_any_account_in_normal_run(cfg):
@@ -443,13 +447,31 @@ def check_update(cfg):
     check_update_on_start(cfg.common)
 
 
-def main():
-    global_usage_counter_name = "global_count"
-    user_usage_counter_name = "user_count/{}".format(get_uuid())
+def show_usage():
+    _show_head_line("从2020-10-26至今小助手使用情况概览")
 
+    user_all_usage, user_today_usage = get_count(my_usage_counter_name, 'all'), get_count(my_usage_counter_name, get_today())
+    global_all_usage, global_today_usage = get_count(global_usage_counter_name, 'all'), get_count(global_usage_counter_name, get_today())
+    all_user, today_user = get_record_count_name_start_with(user_usage_counter_name_prefix, 'all'), get_record_count_name_start_with(user_usage_counter_name_prefix, get_today())
+
+    heads = ["计数对象", "累积", "今日", "本周", "本月", "本年"]
+    periods = ['all', util.get_today(), util.get_week(), util.get_month(), util.get_year()]
+    colSizes = [16, 8, 8, 8, 8, 8]
+    rows = [
+        ["本机使用次数", *[get_count(my_usage_counter_name, period) for period in periods]],
+        ["所有用户使用次数", *[get_count(global_usage_counter_name, period) for period in periods]],
+        ["活跃用户数", *[get_record_count_name_start_with(user_usage_counter_name_prefix, period) for period in periods]],
+    ]
+
+    logger.info(tableify(heads, colSizes))
+    for row in rows:
+        logger.info(color("fg_bold_cyan") + tableify(row, colSizes))
+
+
+def main():
     increase_counter(global_usage_counter_name)
     increase_counter("version/ver{} {}".format(now_version, ver_time))
-    increase_counter(user_usage_counter_name)
+    increase_counter(my_usage_counter_name)
 
     # 最大化窗口
     logger.info("尝试最大化窗口，打包exe可能会运行的比较慢")
@@ -497,12 +519,8 @@ def main():
 
     increase_counter("complete_count")
 
-    user_all_usage, user_today_usage = get_count(user_usage_counter_name, 'all'), get_count(user_usage_counter_name, get_today())
-    global_all_usage, global_today_usage = get_count(global_usage_counter_name, 'all'), get_count(global_usage_counter_name, get_today())
-    logger.warning(("从2020-10-26至今，"
-                    "你已累积使用小助手{}次，今日共使用{}次；"
-                    "所有用户已累积使用小助手{}次，今日共使用{}次。"
-                    ).format(user_all_usage, user_today_usage, global_all_usage, global_today_usage))
+    # 显示小助手的使用概览
+    show_usage()
 
     # 全部账号操作完成后，检查更新
     check_update(cfg)

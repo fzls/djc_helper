026fa78bf4e1b93cbd2a13a30a454aa9c0d9ab15
风之凌殇 <fzls.zju@gmail.com>
2020-10-26 01:58:51 +0800

    增加一个各版本的活跃用户数

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ from util import *
 from version import *
 
 global_usage_counter_name = "global_count"
+this_version_global_usage_counter_name = "version/ver{} {}".format(now_version, ver_time)
 user_usage_counter_name_prefix = "user_count"
 my_usage_counter_name = "{}/{}".format(user_usage_counter_name_prefix, get_uuid())
+this_version_user_usage_prefix = "version_user_usage/{}".format(now_version)
+this_version_my_usage_counter_name = "{}/{}".format(this_version_user_usage_prefix, get_uuid())
 
 
 def has_any_account_in_normal_run(cfg):
@@ -456,11 +459,13 @@ def show_usage():
 
     heads = ["计数对象", "累积", "今日", "本周", "本月", "本年"]
     periods = ['all', util.get_today(), util.get_week(), util.get_month(), util.get_year()]
-    colSizes = [16, 8, 8, 8, 8, 8]
+    colSizes = [20, 8, 8, 8, 8, 8]
     rows = [
         ["本机使用次数", *[get_count(my_usage_counter_name, period) for period in periods]],
+        ["当前版本使用次数", *[get_count(this_version_global_usage_counter_name, period) for period in periods]],
         ["所有用户使用次数", *[get_count(global_usage_counter_name, period) for period in periods]],
-        ["活跃用户数", *[get_record_count_name_start_with(user_usage_counter_name_prefix, period) for period in periods]],
+        ["当前版本活跃用户数", *[get_record_count_name_start_with(this_version_user_usage_prefix, period) for period in periods]],
+        ["所有版本活跃用户数", *[get_record_count_name_start_with(user_usage_counter_name_prefix, period) for period in periods]],
     ]
 
     logger.info(tableify(heads, colSizes))
@@ -470,8 +475,9 @@ def show_usage():
 
 def main():
     increase_counter(global_usage_counter_name)
-    increase_counter("version/ver{} {}".format(now_version, ver_time))
     increase_counter(my_usage_counter_name)
+    increase_counter(this_version_global_usage_counter_name)
+    increase_counter(this_version_my_usage_counter_name)
 
     # 最大化窗口
     logger.info("尝试最大化窗口，打包exe可能会运行的比较慢")

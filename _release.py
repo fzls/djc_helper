abf0dd63938199578fa9ee26a461048ef23c7e15
风之凌殇 <fzls.zju@gmail.com>
2020-11-03 20:48:48 +0800

    重构发布脚本，将打包流程抽取出来

--- _release.py
+++ _release.py
@@ -9,6 +9,7 @@ from log import logger
 from upload_lanzouyun import Uploader
 from util import maximize_console
 from version import now_version
+from _package import package
 
 # 最大化窗口
 maximize_console()
@@ -32,60 +33,16 @@ logger.info("开始发布版本 {}".format(version))
 dir_src = os.path.realpath('.')
 dir_all_release = os.path.realpath(os.path.join("releases"))
 release_dir_name = "DNF蚊子腿小助手_{version}_by风之凌殇".format(version=version)
-dir_current_release = os.path.realpath(os.path.join(dir_all_release, release_dir_name))
-path_bz = os.path.join(dir_src, "bandizip_portable", "bz.exe")
+release_7z_name = '{}.7z'.format(release_dir_name)
 
 # ---------------构建
 # 调用构建脚本
 os.chdir(dir_src)
 subprocess.call(["python", "_build.py"])
 
-# ---------------复制文件到目标目录
+# ---------------打包
 os.chdir(dir_src)
-
-# 需要复制的文件与目录
-files_to_copy = []
-reg_wantted_file = r'.*\.(toml|md|txt|png|docx|url)$'
-for file in os.listdir('.'):
-    if not re.search(reg_wantted_file, file, flags=re.IGNORECASE):
-        continue
-    files_to_copy.append(file)
-files_to_copy.extend([
-    "config.toml.example",
-    "DNF蚊子腿小助手.exe",
-    "DNF蚊子腿小助手配置工具.bat",
-    "bandizip_portable",
-    "reference_data",
-    "chromedriver_85.0.4183.87.exe",
-    "public_key.der",
-    "使用教程",
-    "npp_portable",
-])
-files_to_copy = sorted(files_to_copy)
-
-# 确保发布根目录存在
-if not os.path.isdir(dir_all_release):
-    os.mkdir(dir_all_release)
-# 并清空当前的发布版本目录
-shutil.rmtree(dir_current_release, ignore_errors=True)
-os.mkdir(dir_current_release)
-# 复制文件与目录过去
-logger.info("将以下内容从{}复制到{}".format(dir_src, dir_current_release))
-for filename in files_to_copy:
-    source = os.path.join(dir_src, filename)
-    destination = os.path.join(dir_current_release, filename)
-    if os.path.isdir(filename):
-        logger.info("拷贝目录 {}".format(filename))
-        shutil.copytree(source, destination)
-    else:
-        logger.info("拷贝文件 {}".format(filename))
-        shutil.copyfile(source, destination)
-
-# 压缩打包
-os.chdir(dir_all_release)
-logger.info("开始压缩打包")
-release_7z_name = '{}.7z'.format(release_dir_name)
-subprocess.call([path_bz, 'c', '-y', '-r', '-aoa', '-fmt:7z', '-l:9', release_7z_name, release_dir_name])
+package(dir_src, dir_all_release, release_dir_name, release_7z_name)
 
 # ---------------上传到蓝奏云
 logger.info("开始上传到蓝奏云")

0420f422b6b162311c01a36b6f8b8fa27d665bc8
风之凌殇 <fzls.zju@gmail.com>
2020-11-03 21:06:57 +0800

    调整发布时调用build脚本的方式

--- _build.py
+++ _build.py
@@ -5,75 +5,81 @@ import subprocess
 
 from log import logger
 
-venv_path = ".venv"
-src_name = "main.py"
-exe_name = 'DNF蚊子腿小助手.exe'
-icon = 'DNF蚊子腿小助手.ico'
 
-# 初始化相关路径变量
-pyscript_path = os.path.join(venv_path, "Scripts")
-py_path = os.path.join(pyscript_path, "python")
-pip_path = os.path.join(pyscript_path, "pip")
-pyinstaller_path = os.path.join(pyscript_path, "pyinstaller")
+def build():
+    venv_path = ".venv"
+    src_name = "main.py"
+    exe_name = 'DNF蚊子腿小助手.exe'
+    icon = 'DNF蚊子腿小助手.ico'
 
-logger.info("尝试初始化venv环境")
-subprocess.call([
-    "python",
-    "-m",
-    "venv",
-    venv_path,
-])
+    # 初始化相关路径变量
+    pyscript_path = os.path.join(venv_path, "Scripts")
+    py_path = os.path.join(pyscript_path, "python")
+    pip_path = os.path.join(pyscript_path, "pip")
+    pyinstaller_path = os.path.join(pyscript_path, "pyinstaller")
 
-logger.info("将使用.venv环境进行编译")
+    logger.info("尝试初始化venv环境")
+    subprocess.call([
+        "python",
+        "-m",
+        "venv",
+        venv_path,
+    ])
 
-logger.info("尝试更新pip")
-subprocess.call([
-    py_path,
-    "-m",
-    "pip",
-    "install",
-    "--upgrade",
-    "pip",
-])
+    logger.info("将使用.venv环境进行编译")
 
-logger.info("尝试安装依赖库和pyinstaller")
-subprocess.call([
-    pip_path,
-    "install",
-    "-i",
-    "https://pypi.doubanio.com/simple",
-    "-r",
-    "requirements.txt",
-    "wheel",
-    "pyinstaller",
-])
+    logger.info("尝试更新pip")
+    subprocess.call([
+        py_path,
+        "-m",
+        "pip",
+        "install",
+        "--upgrade",
+        "pip",
+    ])
 
-logger.info("安装pywin32_postinstall")
-subprocess.call([
-    py_path,
-    os.path.join(pyscript_path, "pywin32_postinstall.py"),
-    "-install",
-])
+    logger.info("尝试安装依赖库和pyinstaller")
+    subprocess.call([
+        pip_path,
+        "install",
+        "-i",
+        "https://pypi.doubanio.com/simple",
+        "-r",
+        "requirements.txt",
+        "wheel",
+        "pyinstaller",
+    ])
 
-logger.info("开始编译 {}".format(exe_name))
+    logger.info("安装pywin32_postinstall")
+    subprocess.call([
+        py_path,
+        os.path.join(pyscript_path, "pywin32_postinstall.py"),
+        "-install",
+    ])
 
-cmd_build = [
-    pyinstaller_path,
-    '--icon', icon,
-    '--name', exe_name,
-    '-F',
-    src_name,
-]
+    logger.info("开始编译 {}".format(exe_name))
 
-subprocess.call(cmd_build)
+    cmd_build = [
+        pyinstaller_path,
+        '--icon', icon,
+        '--name', exe_name,
+        '-F',
+        src_name,
+    ]
 
-logger.info("编译结束，进行善后操作")
-# 复制二进制
-shutil.copyfile(os.path.join("dist", exe_name), exe_name)
-# 删除临时文件
-for directory in ["build", "dist", "__pycache__"]:
-    shutil.rmtree(directory, ignore_errors=True)
-for file in ["{}.spec".format(exe_name)]:
-    os.remove(file)
+    subprocess.call(cmd_build)
 
-logger.info("done")
+    logger.info("编译结束，进行善后操作")
+    # 复制二进制
+    shutil.copyfile(os.path.join("dist", exe_name), exe_name)
+    # 删除临时文件
+    for directory in ["build", "dist", "__pycache__"]:
+        shutil.rmtree(directory, ignore_errors=True)
+    for file in ["{}.spec".format(exe_name)]:
+        os.remove(file)
+
+    logger.info("done")
+
+
+if __name__ == '__main__':
+    build()

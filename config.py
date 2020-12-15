4f9de4d0f7b4ead1bca5390f08ec8f92b9242c7c
风之凌殇 <fzls.zju@gmail.com>
2020-12-15 16:29:18 +0800

    修正集卡礼包配置说明

--- config.py
+++ config.py
@@ -79,7 +79,7 @@ class ArkLotteryConfig(ConfigInterface):
         self.lucky_dnf_role_id = ""  # 角色ID，不知道时可以填写区服ID，该数值留空，这样处理到抽卡的时候会用黄色字体打印出来信息
         # 是否消耗所有卡牌来抽奖（建议在兑换完所有礼包后再开启这个功能）
         self.cost_all_cards_and_do_lottery = False
-        # 尝试领取礼包的次数：全民竞速礼包=28592，即刷即得礼包=28593，直播福利礼包=28594，幸运礼包=28583
+        # 尝试领取礼包的次数：全民竞速礼包=28592，即刷即得礼包=28593，直播福利礼包=28594，幸运礼包=[依次执行 28610、28609、28583、28611、28612]
         self.take_awards = []  # type: List[ArkLotteryAwardConfig]
 
         # 是否展示在概览界面

423cfe9ca370dd8b5ffa4c9cbdefb8e0b84e2bb6
风之凌殇 <fzls.zju@gmail.com>
2020-12-18 17:27:33 +0800

    增加检测自动运行时未配置工作目录的情况

--- game_info.py
+++ game_info.py
@@ -1,4 +1,5 @@
 import json
+import os
 import subprocess
 from sys import exit
 
@@ -6,16 +7,29 @@ import win32api
 import win32con
 
 from dao import GameInfo
+from log import logger
 
 name_2_game_info_map = {}
 code_2_game_info_map = {}
 
-with open("reference_data/djc_biz_list.json", "r", encoding="utf-8") as f:
-    raw_data = json.load(f)
-    for game_data in raw_data["data"]:
-        gameInfo = GameInfo(game_data)
-        name_2_game_info_map[game_data["bizName"]] = GameInfo(game_data)
-        code_2_game_info_map[game_data["bizCode"]] = GameInfo(game_data)
+try:
+    with open("reference_data/djc_biz_list.json", "r", encoding="utf-8") as f:
+        raw_data = json.load(f)
+        for game_data in raw_data["data"]:
+            gameInfo = GameInfo(game_data)
+            name_2_game_info_map[game_data["bizName"]] = GameInfo(game_data)
+            code_2_game_info_map[game_data["bizCode"]] = GameInfo(game_data)
+except FileNotFoundError as e:
+    logger.error((
+        "未找到djc配置文件，是否是下述两种情况之一\n"
+        "   1. 直接在压缩包中运行exe\n"
+        "   2. 未在解压缩出的目录中运行exe\n"
+        "   3. 使用任务计划程序自动运行时未配置工作目录（也就是配置任务时的【起始于(可选)】配置项。需要确保设置为exe所在目录\n"
+        "\n"
+        "请按照上述提示调整后重试\n"
+    ), exc_info=e)
+    os.system("PAUSE")
+    exit(-1)
 
 
 def get_game_info(name):

9c06826e40b9e87efd9fe0930083bf3e6e724625
风之凌殇 <fzls.zju@gmail.com>
2020-12-25 01:58:35 +0800

    当添加了多个活动的版本发布时，弹出一条恰饭信息

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ def main():
     logger.info("尝试最大化窗口，打包exe可能会运行的比较慢")
     maximize_console()
 
+    show_qiafan_message_box_on_every_big_version("v4.6.1")
+
     logger.warning("开始运行DNF蚊子腿小助手，ver={} {}，powered by {}".format(now_version, ver_time, author))
     logger.warning(color("fg_bold_cyan") + "如果觉得我的小工具对你有所帮助，想要支持一下我的话，可以帮忙宣传一下或打开支持一下.png，扫码打赏哦~")
 

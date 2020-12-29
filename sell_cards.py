09c3d6314aee06f85fb171e57ea2a4f085415dcc
风之凌殇 <fzls.zju@gmail.com>
2020-12-29 11:04:02 +0800

    更新卖卡脚本

--- sell_cards.py
+++ sell_cards.py
@@ -8,7 +8,11 @@ if __name__ == '__main__':
     load_config("config.toml", "config.toml.local")
     cfg = config()
 
-    indexes = [1]
+    # 12.29 送卡片次数
+    # 1 - 3
+    # 2 - 4
+    # 3 - 2
+    indexes = [3]
 
     for idx in indexes:  # 从1开始，第i个
         account_config = cfg.account_configs[idx - 1]
@@ -19,24 +23,27 @@ if __name__ == '__main__':
         djcHelper.get_bind_role_list()
 
         djcHelper.fetch_pskey()
-        targetQQ = "240543689"
+        targetQQ = ""
         cards_to_send = [
-            # ("哪种都能领奖励", 4)
+            # ("主播趣味来打团", 1),
+            # ("哪种都能领奖励", 1),
         ]
         for name, count in cards_to_send:
             for i in range(count):
                 djcHelper.send_card_by_name(name, targetQQ)
 
 # -----------具体卡片相关信息---------------
-# 巅峰大佬刷竞速 {'name': '巅峰大佬刷竞速', 'id': 118409, 'prizeId': 44460, 'lotterySwitchId': 28608, 'index': '1-1'}
-# 主播趣味来打团 {'name': '主播趣味来打团', 'id': 118408, 'prizeId': 44459, 'lotterySwitchId': 28607, 'index': '1-2'}
-# BOSS机制全摸透 {'name': 'BOSS机制全摸透', 'id': 118407, 'prizeId': 44458, 'lotterySwitchId': 28606, 'index': '1-3'}
-# 萌新翻身把歌唱 {'name': '萌新翻身把歌唱', 'id': 118406, 'prizeId': 44457, 'lotterySwitchId': 28605, 'index': '1-4'}
-# 四人竞速希洛克 {'name': '四人竞速希洛克', 'id': 118405, 'prizeId': 44456, 'lotterySwitchId': 28604, 'index': '2-1'}
-# 普通困难任你选 {'name': '普通困难任你选', 'id': 118404, 'prizeId': 44455, 'lotterySwitchId': 28603, 'index': '2-2'}
-# 哪种都能领奖励 {'name': '哪种都能领奖励', 'id': 118403, 'prizeId': 44454, 'lotterySwitchId': 28602, 'index': '2-3'}
-# 点击报名薅大礼 {'name': '点击报名薅大礼', 'id': 118402, 'prizeId': 44453, 'lotterySwitchId': 28601, 'index': '2-4'}
-# 打团就可赢好礼 {'name': '打团就可赢好礼', 'id': 118401, 'prizeId': 44452, 'lotterySwitchId': 28600, 'index': '3-1'}
-# 报名即可领豪礼 {'name': '报名即可领豪礼', 'id': 118400, 'prizeId': 44451, 'lotterySwitchId': 28599, 'index': '3-2'}
-# 直播Q币抽不停  {'name': '直播Q币抽不停', 'id': 118399, 'prizeId': 44450, 'lotterySwitchId': 28598, 'index': '3-3'}
-# 决赛红包等着你 {'name': '决赛红包等着你', 'id': 118398, 'prizeId': 44449, 'lotterySwitchId': 28597, 'index': '3-4'}
+# '1-1' 巅峰大佬刷竞速 {'name': '巅峰大佬刷竞速', 'id': 118409, 'prizeId': 44460, 'lotterySwitchId': 28608, 'index': '1-1'}
+# '1-2' 主播趣味来打团 {'name': '主播趣味来打团', 'id': 118408, 'prizeId': 44459, 'lotterySwitchId': 28607, 'index': '1-2'}
+# '1-3' BOSS机制全摸透 {'name': 'BOSS机制全摸透', 'id': 118407, 'prizeId': 44458, 'lotterySwitchId': 28606, 'index': '1-3'}
+# '1-4' 萌新翻身把歌唱 {'name': '萌新翻身把歌唱', 'id': 118406, 'prizeId': 44457, 'lotterySwitchId': 28605, 'index': '1-4'}
+
+# '2-1' 四人竞速希洛克 {'name': '四人竞速希洛克', 'id': 118405, 'prizeId': 44456, 'lotterySwitchId': 28604, 'index': '2-1'}
+# '2-2' 普通困难任你选 {'name': '普通困难任你选', 'id': 118404, 'prizeId': 44455, 'lotterySwitchId': 28603, 'index': '2-2'}
+# '2-3' 哪种都能领奖励 {'name': '哪种都能领奖励', 'id': 118403, 'prizeId': 44454, 'lotterySwitchId': 28602, 'index': '2-3'}
+# '2-4' 点击报名薅大礼 {'name': '点击报名薅大礼', 'id': 118402, 'prizeId': 44453, 'lotterySwitchId': 28601, 'index': '2-4'}
+
+# '3-1' 打团就可赢好礼 {'name': '打团就可赢好礼', 'id': 118401, 'prizeId': 44452, 'lotterySwitchId': 28600, 'index': '3-1'}
+# '3-2' 报名即可领豪礼 {'name': '报名即可领豪礼', 'id': 118400, 'prizeId': 44451, 'lotterySwitchId': 28599, 'index': '3-2'}
+# '3-3' 直播Q币抽不停  {'name': '直播Q币抽不停', 'id': 118399, 'prizeId': 44450, 'lotterySwitchId': 28598, 'index': '3-3'}
+# '3-4' 决赛红包等着你 {'name': '决赛红包等着你', 'id': 118398, 'prizeId': 44449, 'lotterySwitchId': 28597, 'index': '3-4'}

b133e70a1d741ffe86f437f62bb3bf32b14ecb8b
风之凌殇 <fzls.zju@gmail.com>
2021-01-01 16:43:37 +0800

    移除过期的编年史活动

--- djc_helper.py
+++ djc_helper.py
@@ -369,9 +369,6 @@ class DjcHelper:
         # 腾讯游戏信用相关礼包
         self.get_credit_xinyue_gift()
 
-        # dnf助手编年史活动
-        self.dnf_helper_chronicle()
-
         # hello语音网页礼包兑换
         self.hello_voice()
 
@@ -433,6 +430,9 @@ class DjcHelper:
 
         # 阿拉德勇士征集令
         self.dnf_warriors_call()
+
+        # dnf助手编年史活动
+        self.dnf_helper_chronicle()
 
     # --------------------------------------------道聚城--------------------------------------------
     def djc_operations(self):

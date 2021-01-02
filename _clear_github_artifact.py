b7768671f385d1d56319241b352fde63aaca67da
风之凌殇 <fzls.zju@gmail.com>
2021-01-02 17:35:48 +0800

    需要在函数内切换cwd的，统一在结束时切换回去

--- _clear_github_artifact.py
+++ _clear_github_artifact.py
@@ -3,10 +3,13 @@ import shutil
 
 
 def clear_github_artifact(dir_all_release, dir_github_action_artifact):
+    old_cwd = os.getcwd()
     os.chdir(dir_all_release)
 
     shutil.rmtree(dir_github_action_artifact, ignore_errors=True)
     os.mkdir(dir_github_action_artifact)
+
+    os.chdir(old_cwd)
 
 
 if __name__ == '__main__':

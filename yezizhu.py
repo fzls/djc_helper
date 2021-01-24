dc9222d3a10bb5dcc626eb856e7135f39295f788
风之凌殇 <fzls.zju@gmail.com>
2021-01-24 19:31:57 +0800

    增加打印积分进度

--- yezizhu.py
+++ yezizhu.py
@@ -15,6 +15,22 @@ prize_name = quote_plus("春节套*1")
 processor_count = 2 * multiprocessing.cpu_count()
 pool = Pool(processor_count)
 
+headers = {
+    "Proxy-Connection": "keep-alive",
+    "Accept": "application/json, text/javascript, */*; q=0.01",
+    "DNT": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    "Origin": "http://dnf.yzz.cn",
+    "Referer": "http://dnf.yzz.cn/",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7,en-GB;q=0.6,ja;q=0.5",
+}
+
+post_headers = {
+    **headers,
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+}
+
 
 def try_exchange():
     logger.info("开始运行兑换叶子猪春节套脚本")
@@ -32,9 +48,16 @@ def try_exchange():
         exchange(startTime, endTime)
 
 
+def query_user_info():
+    url = f"http://dnf.api.yzz.cn/activity/userInfo?mobile={mobile}"
+    res = requests.get(url, headers=headers).json()
+
+    return "账号({mobile}) 当前积分为{point_amount}，已使用{point_used}，已领取的奖励为：{prize_jfcj} {prize_jfdh}(查询于{ts})".format(**res["data"], ts=datetime.now())
+
+
 def query_info():
     url = "http://dnf.api.yzz.cn/activity/getExchangeList"
-    res = requests.get(url).json()
+    res = requests.get(url, headers=headers).json()
 
     heads = ["id", "名称", "积分", "启用", "库存", "已发放", "总计"]
     colSizes = [3, 25, 6, 6, 6, 6, 6]
@@ -49,15 +72,25 @@ def query_info():
 
 def exchange(startTime, endTime):
     waitTime = 0.001
+    show_progress_progress = 0
     show_progress_delta = 0.1
+    update_user_info_progress = 0
+    update_user_info_delta = 60
+
+    latest_user_info = query_user_info()
 
     logger.info(color("bold_yellow") + f"本轮开始时间为{startTime}，结束时间为{endTime}，请求等待间隔为{waitTime}s，预计将尝试{(endTime - startTime).seconds // waitTime}次")
-    progress = 0
     while datetime.now() < startTime:
-        progress += waitTime
-        if progress >= show_progress_delta:
-            print(f"\r当前时间为{datetime.now()}...，还需要等待{startTime - datetime.now()}才会开始尝试", end='')
-            progress -= show_progress_delta
+        show_progress_progress += waitTime
+        update_user_info_progress += waitTime
+
+        if show_progress_progress >= show_progress_delta:
+            if update_user_info_progress >= update_user_info_delta:
+                latest_user_info = query_user_info()
+                update_user_info_progress -= update_user_info_delta
+
+            print(color("bold_cyan") + f"\r{latest_user_info}。当前时间为{datetime.now()}...，还需要等待{startTime - datetime.now()}才会开始尝试。", end='')
+            show_progress_progress -= show_progress_delta
         time.sleep(waitTime)
     print("\r", end='')
 
@@ -85,19 +118,8 @@ def exchange(startTime, endTime):
 
 def do_exchange(index=0):
     url = "http://dnf.api.yzz.cn/activity/exchange"
-    headers = {
-        "Proxy-Connection": "keep-alive",
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "DNT": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-        "Origin": "http://dnf.yzz.cn",
-        "Referer": "http://dnf.yzz.cn/",
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "en,zh-CN;q=0.9,zh;q=0.8,zh-TW;q=0.7,en-GB;q=0.6,ja;q=0.5",
-    }
 
-    res = requests.post(url, data=f"mobile={mobile}&prize_name={prize_name}", headers=headers)
+    res = requests.post(url, data=f"mobile={mobile}&prize_name={prize_name}", headers=post_headers)
     logger.info(f"请求序号{index:5d}: {res.json()}")
     return res.json()
 

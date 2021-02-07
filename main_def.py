76e962a05309f418846b92e23f877882d4344c79
风之凌殇 <fzls.zju@gmail.com>
2021-02-08 01:11:23 +0800

    如果从未购买过，过期时间改为免费开始时间点

--- main_def.py
+++ main_def.py
@@ -742,6 +742,9 @@ def get_user_buy_info(cfg: Config):
         max_present_times = datetime.timedelta(days=2 * 31)
 
         free_start_time = parse_time("2021-02-07 00:00:00")
+        if user_buy_info.total_buy_month == 0:
+            # 如果从未购买过，过期时间改为免费开始时间点
+            user_buy_info.expire_at = format_time(free_start_time)
 
         now = datetime.datetime.now()
         since_start_time = now - free_start_time

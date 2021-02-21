ec7185ad89a15f24483c58e64f883eafb5982956
风之凌殇 <fzls.zju@gmail.com>
2021-02-21 19:17:19 +0800

    心悦战场增加通用参数

--- djc_helper.py
+++ djc_helper.py
@@ -1090,8 +1090,8 @@ class DjcHelper:
             score, ysb, xytype, specialMember, username, usericon = "0", "0", "1", "0", "查询失败了", ""
         return XinYueInfo(score, ysb, xytype, specialMember, username, usericon)
 
-    def xinyue_battle_ground_op(self, ctx, iFlowId, package_id="", print_res=True, lqlevel=1, teamid=""):
-        return self.xinyue_op(ctx, self.urls.iActivityId_xinyue_battle_ground, iFlowId, package_id, print_res, lqlevel, teamid)
+    def xinyue_battle_ground_op(self, ctx, iFlowId, package_id="", print_res=True, lqlevel=1, teamid="", **extra_params):
+        return self.xinyue_op(ctx, self.urls.iActivityId_xinyue_battle_ground, iFlowId, package_id, print_res, lqlevel, teamid, **extra_params)
 
     def xinyue_op(self, ctx, iActivityId, iFlowId, package_id="", print_res=True, lqlevel=1, teamid="", **extra_params):
         # 网站上特邀会员不论是游戏家G几，调用doAction(flowId,level)时level一律传1，而心悦会员则传入实际的567对应心悦123

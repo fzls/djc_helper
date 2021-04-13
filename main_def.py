6d68edfd4820ef4642cd632d33c8e7603e5707bc
风之凌殇 <fzls.zju@gmail.com>
2021-04-13 16:37:17 +0800

    增加注释

--- main_def.py
+++ main_def.py
@@ -409,9 +409,10 @@ def show_accounts_status(cfg, ctx):
 
             rows.append(get_account_status(idx, account_config, cfg.common))
 
+    # 打印结果
     heads = ["序号", "账号名", "启用状态", "聚豆余额", "聚豆历史总数", "心悦类型", "成就点", "勇士币", "心悦组队", "赛利亚", "心悦G分", "编年史", "年史碎片"]
     colSizes = [4, 12, 8, 8, 12, 8, 6, 6, 16, 12, 8, 14, 8]
-    
+
     logger.info(tableify(heads, colSizes))
     for row in rows:
         logger.info(color("fg_bold_green") + tableify(row, colSizes, need_truncate=True))

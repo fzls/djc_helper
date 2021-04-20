92c1214a10b0ba3e6336b579d8dd43b84bd031e5
风之凌殇 <fzls.zju@gmail.com>
2021-04-20 10:41:23 +0800

    调整付费卡住提示

--- main_def.py
+++ main_def.py
@@ -902,9 +902,7 @@ def has_buy_auto_updater_dlc(cfg: Config):
 
 
 def get_user_buy_info(cfg: Config):
-    logger.info("如果卡在这里不能动，说明你无法访问蓝奏云的网站，比如https://fzls.lanzous.com/s/djc-helper，一般是你网络有问题，比如DNS，请百度 修改DNS为腾讯、百度、阿里、谷歌的DNS之一")
-    logger.info("也有可能是蓝奏云挂掉了哦-。-，只能先等等再运行了")
-    logger.info("如果要反馈，建议先看看网盘里是否有新版本~")
+    logger.info("如果卡在这里不能动，请先看看网盘里是否有新版本~ 如果新版本仍无法解决，可加群反馈~ 链接：https://fzls.lanzous.com/s/djc-helper")
     user_buy_info = _get_user_buy_info(cfg)
     # 购买过dlc的用户可以获得两个月免费使用付费功能的时长
     if has_buy_auto_updater_dlc(cfg):

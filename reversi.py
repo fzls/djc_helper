bee757d52279efc95fa0d9ba3914a4fe71b3136a
风之凌殇 <fzls.zju@gmail.com>
2021-05-10 13:25:17 +0800

    增加预搜索流程

--- reversi.py
+++ reversi.py
@@ -87,6 +87,10 @@ class Reversi(QWidget):
         self.game_start_time = datetime.now()
         self.game_restarted = False
 
+        self.enable_presearch = True
+        self.ai_dfs_max_choice_per_depth = 5
+        self.ai_dfs_presearch_depth = 2
+
         self.ai_dfs_max_depth = 4
         self.ai_min_decision_seconds = timedelta(seconds=0.5)
         self.ai_max_decision_time = timedelta(seconds=26)
@@ -456,7 +460,7 @@ class Reversi(QWidget):
 
         return res[0]
 
-    def ai_min_max_dfs(self, depth, valid_cells: List[Tuple[int, int]], ai_step_cell, alpha, beta) -> Tuple[Optional[Tuple[int, int]], int]:
+    def ai_min_max_dfs(self, depth, valid_cells: List[Tuple[int, int]], ai_step_cell, alpha, beta, presearch=False) -> Tuple[Optional[Tuple[int, int]], int]:
         if datetime.now() - self.last_update_time >= timedelta(seconds=1 / 60):
             since_start = datetime.now() - self.ai_start_time
             remaining_time = (self.ai_max_decision_time - since_start)
@@ -481,51 +485,89 @@ class Reversi(QWidget):
         best_next_move = None
 
         if len(valid_cells) != 0:
-            # 按照权重先排个序
-            valid_cells = sorted(valid_cells, key=lambda v: weight_map[v[0] - 1][v[1] - 1], reverse=need_reverse_weights)
-            for idx, next_move_index in enumerate(valid_cells):
-                next_move_row_index, next_move_col_index = next_move_index
+            # 子搜索流程（实际搜索和预搜索将共用该逻辑）
+            def subsearch(valid_cells, alpha, beta, next_depth, presearch=False, subresult_cb=None):
+                best_next_move = None
 
-                revoke_op = self.put_cell(next_move_row_index, next_move_col_index, ai_probe=True)
+                for idx, next_move_index in enumerate(valid_cells):
+                    next_move_row_index, next_move_col_index = next_move_index
 
-                next_depth_best_move = self.ai_min_max_dfs(depth + 1, self.get_valid_cells(self.current_step_cell()), ai_step_cell, alpha, beta)
-                if next_depth_best_move is None:
-                    continue
+                    revoke_op = self.put_cell(next_move_row_index, next_move_col_index, ai_probe=True)
 
-                next_depth_min_max_score = next_depth_best_move[1]
+                    next_depth_best_move = self.ai_min_max_dfs(next_depth, self.get_valid_cells(self.current_step_cell()), ai_step_cell, alpha, beta, presearch)
+                    if next_depth_best_move is None:
+                        continue
 
-                next_move = ((next_move_row_index, next_move_col_index), next_depth_min_max_score)
-                if best_next_move is None:
-                    best_next_move = next_move
-                else:
-                    best_next_move = min_max(best_next_move, next_move, key=lambda v: v[1])
+                    next_depth_min_max_score = next_depth_best_move[1]
 
+                    next_move = ((next_move_row_index, next_move_col_index), next_depth_min_max_score)
+                    if best_next_move is None:
+                        best_next_move = next_move
+                    else:
+                        best_next_move = min_max(best_next_move, next_move, key=lambda v: v[1])
 
-                revoke_op()
+                    revoke_op()
 
-                # 更新alpha、beta
-                if self.step_cell == ai_step_cell:
-                    if next_depth_min_max_score > alpha:
-                        alpha = next_depth_min_max_score
-                else:
-                    if next_depth_min_max_score < beta:
-                        beta = next_depth_min_max_score
+                    # 更新alpha、beta
+                    if self.step_cell == ai_step_cell:
+                        if next_depth_min_max_score > alpha:
+                            alpha = next_depth_min_max_score
+                    else:
+                        if next_depth_min_max_score < beta:
+                            beta = next_depth_min_max_score
 
-                # 剪枝
-                if alpha >= beta:
-                    logger.debug(f"剪枝 alpha={alpha}, beta={beta}")
-                    break
+                    # 子节点结果回调
+                    if subresult_cb is not None:
+                        subresult_cb(idx, next_depth_min_max_score)
 
-                # 如果运行时间超限，停止处理
-                since_start = datetime.now() - self.ai_start_time
-                if since_start >= self.ai_max_decision_time:
-                    logger.info(f"depth={depth}/{self.ai_dfs_max_depth} valid_cells={idx + 1}/{len(valid_cells)} 等待时间已达到{since_start}，将强制停止搜索")
-                    break
+                    # 剪枝
+                    if alpha >= beta:
+                        logger.debug(f"剪枝 alpha={alpha}, beta={beta}")
+                        break
 
-                # 如果重开了，停止处理
-                if self.game_restarted:
-                    logger.info(f"游戏重开，将强制停止搜索")
-                    break
+                    # 如果运行时间超限，停止处理
+                    since_start = datetime.now() - self.ai_start_time
+                    if since_start >= self.ai_max_decision_time:
+                        logger.info(f"depth={depth}/{self.ai_dfs_max_depth} valid_cells={idx + 1}/{len(valid_cells)} 等待时间已达到{since_start}，将强制停止搜索")
+                        break
+
+                    # 如果重开了，停止处理
+                    if self.game_restarted:
+                        logger.info(f"游戏重开，将强制停止搜索")
+                        break
+
+                return best_next_move
+
+            need_presearch = self.enable_presearch and \
+                not presearch and \
+                len(valid_cells) > self.ai_dfs_max_choice_per_depth and \
+                depth + self.ai_dfs_presearch_depth < self.ai_dfs_max_depth
+
+            if need_presearch:
+                # 预计算若干层得到各落子的评分，按照该评分排序
+                # 预搜索时本层实际不需要剪枝，因为目的是为了计算出各个子节点的排序权重
+                presearch_alpha, presearch_beta = -0x7fffffff, 0x7fffffff
+                presearch_depth = self.ai_dfs_max_depth - self.ai_dfs_presearch_depth
+                valid_cells_weights = [0 for cell in valid_cells]
+
+                def subresult_cb(child_idx, next_depth_min_max_score):
+                    valid_cells_weights[child_idx] = next_depth_min_max_score
+
+                subsearch(valid_cells, presearch_alpha, presearch_beta, presearch_depth, presearch=True, subresult_cb=subresult_cb)
+
+                # 根据计算出的权重进行排序
+                valid_cells = [cell for weight, cell in sorted(zip(valid_cells_weights, valid_cells), reverse=need_reverse_weights)]
+                # 取前几个
+                valid_cells = valid_cells[:self.ai_dfs_max_choice_per_depth]
+            else:
+                # 以下情况则直接按照权重排序
+                # 1. 如果可选落子数不多
+                # 2. 已经接近叶节点
+                # 3. 预搜索流程
+                valid_cells = sorted(valid_cells, key=lambda v: weight_map[v[0] - 1][v[1] - 1], reverse=need_reverse_weights)
+
+            # 正式开始搜索
+            best_next_move = subsearch(valid_cells, alpha, beta, depth+1)
         else:
             # 本方无可行下子，跳过本轮
             old_step_cell = self.step_cell

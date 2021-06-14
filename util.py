81e02adc4f03ef365838a4c6eb71bd42b1ead7ab
风之凌殇 <fzls.zju@gmail.com>
2021-06-15 04:37:01 +0800

    仅打印最后一次收到的结果相关信息

--- util.py
+++ util.py
@@ -348,11 +348,9 @@ def check_some_exception(e, show_last_process_result=True) -> str:
         ))
 
     if show_last_process_result:
-        from network import last_process_result, last_receive_response
-        if last_process_result is not None:
-            msg += format_msg(f"最近一次处理成功的请求结果为：{last_process_result}", "bold_green")
-        if last_receive_response is not None:
-            msg += format_msg(f"最近一次收到的请求结果为：{last_receive_response}", "bold_cyan")
+        from network import last_response_info
+        if last_response_info is not None:
+            msg += format_msg(f"最近一次收到的请求结果为：{last_response_info}", "bold_cyan")
 
     return msg
 

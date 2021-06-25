3697bfa6b2e0ab3507af584dcbb24c844b6e8d40
风之凌殇 <fzls.zju@gmail.com>
2021-06-25 17:09:29 +0800

    first_run改用复用的db来实现

--- db_def.py
+++ db_def.py
@@ -1,4 +1,5 @@
 from db_new import DbInterface
+from util import parse_time
 
 
 # ----------------- 数据定义 -----------------
@@ -8,6 +9,14 @@ class TestDb(DbInterface):
         super().__init__()
         self.int_val = 1
         self.bool_val = True
+
+
+class FirstRunData(DbInterface):
+    def __init__(self):
+        super().__init__()
+
+    def get_update_at(self):
+        return parse_time(self.update_at)
 
 
 if __name__ == '__main__':

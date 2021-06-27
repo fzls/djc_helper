39ec42fc2188bf1d40cd69c1beb28bece0a66b98
风之凌殇 <fzls.zju@gmail.com>
2021-06-28 01:56:08 +0800

    使用新的db来替代旧的-CacheDB

--- db_new.py
+++ db_new.py
@@ -1,6 +1,6 @@
-from typing import List, Dict
+from typing import List, Dict, Any, Type, Tuple
 
-from db_def import DBInterface
+from db_def import DBInterface, ConfigInterface
 from util import parse_time
 
 
@@ -25,26 +25,26 @@ class WelfareDB(DBInterface):
     def __init__(self):
         super().__init__()
 
-        self.share_code_list = [] # type: List[str]
-        self.exchanged_dict = {} # type: Dict[str, bool]
+        self.share_code_list = []  # type: List[str]
+        self.exchanged_dict = {}  # type: Dict[str, bool]
 
 
 class DianzanDB(DBInterface):
     def __init__(self):
         super().__init__()
 
-        self.day_to_dianzan_count = {} # type: Dict[str, int]
-        self.used_content_ids = [] # type: List[str]
-        self.content_ids = [] # type: List[str]
+        self.day_to_dianzan_count = {}  # type: Dict[str, int]
+        self.used_content_ids = []  # type: List[str]
+        self.content_ids = []  # type: List[str]
 
 
 class CaptchaDB(DBInterface):
     def __init__(self):
         super().__init__()
 
-        self.offset_to_history_succes_count = {} # type: Dict[str, int]
+        self.offset_to_history_succes_count = {}  # type: Dict[str, int]
 
-    def increse_success_count(self, offset:int):
+    def increse_success_count(self, offset: int):
         success_key = str(offset)  # 因为json只支持str作为key，所以需要强转一下，使用时再转回int
         if success_key not in self.offset_to_history_succes_count:
             self.offset_to_history_succes_count[success_key] = 0
@@ -56,8 +56,28 @@ class LoginRetryDB(DBInterface):
     def __init__(self):
         super().__init__()
 
-        self.recommended_first_retry_timeout = 0.0 # type: float
-        self.history_success_timeouts = [] # type: List[float]
+        self.recommended_first_retry_timeout = 0.0  # type: float
+        self.history_success_timeouts = []  # type: List[float]
+
+
+class CacheDB(DBInterface):
+    def __init__(self):
+        super().__init__()
+
+        self.cache = {}  # type: Dict[str, CacheInfo]
+
+    def dict_fields_to_fill(self) -> List[Tuple[str, Type[ConfigInterface]]]:
+        return [
+            ('cache', CacheInfo)
+        ]
+
+
+class CacheInfo(DBInterface):
+    def __init__(self):
+        super().__init__()
+
+        self.value = None  # type: Any
+
 
 if __name__ == '__main__':
     print(DBInterface())

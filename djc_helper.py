37c6ef165c438e50acdcf8f28e28355632435fe4
风之凌殇 <fzls.zju@gmail.com>
2021-07-06 09:37:53 +0800

    更新漫画活动集数

--- djc_helper.py
+++ djc_helper.py
@@ -1680,7 +1680,7 @@ class DjcHelper:
             "774799", "774800",
         ]
         # note: 当前更新至（定期刷新这个值）
-        current_updated = 10
+        current_updated = 11
         for _idx, flowid in enumerate(watch_comic_flowids):
             idx = _idx + 1
             if idx > current_updated:

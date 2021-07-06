976763a8510a97169425e16b1537a975e3cb718d
风之凌殇 <fzls.zju@gmail.com>
2021-07-06 23:48:40 +0800

    配置读取错误，写完日志后继续抛出异常，由顶层异常处理逻辑来捕获

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+import os
 import re
 from multiprocessing import cpu_count
 
@@ -787,15 +788,15 @@ def load_config(config_path="config.toml", local_config_path="config.toml.local"
         g_config.auto_update_config(raw_config)
     except UnicodeDecodeError as error:
         logger.error(color("fg_bold_yellow") + f"{config_path}的编码格式有问题，应为utf-8，如果使用系统自带记事本的话，请下载vscode或notepad++等文本编辑器\n错误信息：{error}\n")
-        exit(0)
+        raise error
     except Exception as error:
         if encoding_error_str in str(error):
             logger.error(color("fg_bold_yellow") + f"{config_path}的编码格式有问题，应为utf-8，如果使用系统自带记事本的话，请下载vscode或notepad++等文本编辑器\n错误信息：{error}\n")
-            exit(0)
+            raise error
 
         logger.error(color("fg_bold_red") + f"读取{config_path}文件出错，是否直接在压缩包中打开了或者toml语法有问题？\n具体出错为：{error}\n" +
                      color("fg_bold_yellow") + "若未完整解压，请先解压。否则请根据上面的英文报错信息，自行百度学习toml的基本语法，然后处理对应行的语法错误（看不懂的话自行用百度翻译或有道翻译）")
-        exit(-1)
+        raise error
 
     # 然后尝试读取本地文件（不受版本管理系统控制）
     try:

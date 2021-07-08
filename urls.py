56078f73a90de921de587f12be90585a373be1e4
风之凌殇 <fzls.zju@gmail.com>
2021-07-08 15:46:58 +0800

    接入新的心悦活动（施工中）

--- urls.py
+++ urls.py
@@ -131,7 +131,7 @@ class Urls:
         self.iActivityId_dnf_ozma = "382419"  # DNF奥兹玛竞速
         self.iActivityId_hello_voice = "366492"  # hello语音奖励兑换
         self.iActivityId_dnf_pk = "370758"  # DNF格斗大赛
-        self.iActivityId_dnf_xinyue_51 = "373457"  # DNF心悦51
+        self.iActivityId_dnf_xinyue = "388970"  # DNF心悦
         self.iActivityId_dnf_strong = "366330"  # DNF强者之路
         self.iActivityId_dnf_comic = "386057"  # DNF&腾讯动漫周年庆合作活动
         self.iActivityId_dnf_13 = "381033"  # DNF十三周年庆双端站点

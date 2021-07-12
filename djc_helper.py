cfa56068d04e79f48194f9fb430e248e0f793308
风之凌殇 <fzls.zju@gmail.com>
2021-07-12 16:33:13 +0800

    增加第12集

--- djc_helper.py
+++ djc_helper.py
@@ -1707,7 +1707,7 @@ class DjcHelper:
             "774799", "774800",
         ]
         # note: 当前更新至（定期刷新这个值）
-        current_updated = 11
+        current_updated = 12
         for _idx, flowid in enumerate(watch_comic_flowids):
             idx = _idx + 1
             if idx > current_updated:

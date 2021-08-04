d24e71843cbbca9f1c4c8988332115e87b142f84
风之凌殇 <fzls.zju@gmail.com>
2021-08-04 19:26:21 +0800

    增加pytest的用例

---
+++ test_util.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -------------------------------
+# File      : test_util
+# DateTime  : 2021/8/4 19:07
+# Author    : Chen Ji
+# Email     : fzls.zju@gmail.com
+# -------------------------------
+from util import *
+
+
+def test_uin2qq():
+    assert uin2qq("o1234567890") == "1234567890"
+    assert uin2qq("o0123456789") == "123456789"
+
+
+def test_is_valid_qq():
+    assert is_valid_qq("123456789")
+    assert not is_valid_qq("a123456789")
+    assert not is_valid_qq("")

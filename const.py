c5f58e83e6de08fee93840d9e2f1dd743f0ef0b4
风之凌殇 <fzls.zju@gmail.com>
2021-08-11 16:25:34 +0800

    增加下载和压缩临时目录地址

--- const.py
+++ const.py
@@ -10,4 +10,12 @@ db_top_dir = ".db"
 if not os.path.isdir(db_top_dir):
     os.mkdir(db_top_dir)
 
+downloads_dir = f"{cached_dir}/downloads"
+if not os.path.isdir(downloads_dir):
+    os.mkdir(downloads_dir)
+
+compressed_temp_dir = f"{cached_dir}/compressed"
+if not os.path.isdir(compressed_temp_dir):
+    os.mkdir(compressed_temp_dir)
+
 guanjia_skey_version = 2

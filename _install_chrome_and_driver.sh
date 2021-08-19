130bf9bd769d2902cdc33f3f08072a7685a6cfd0
风之凌殇 <fzls.zju@gmail.com>
2021-08-19 19:18:20 +0800

    调整安装脚本

--- _install_chrome_and_driver.sh
+++ _install_chrome_and_driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get install unzip
+sudo apt-get install unzip
 
 wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip

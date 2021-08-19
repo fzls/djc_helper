a535de864a6f28a8954bccb6f67dcb5f4f2eac89
风之凌殇 <fzls.zju@gmail.com>
2021-08-19 19:40:37 +0800

    将下载和安装脚本分开，方便github action中进行缓存

---
+++ _download_chrome_and_driver.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get install unzip
+
+wget http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
+wget https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip

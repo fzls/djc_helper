f32325a563168014672e0e55ac84e662edb41172
风之凌殇 <fzls.zju@gmail.com>
2021-08-23 00:58:26 +0800

    更新漫画活动到18集

--- djc_helper.py
+++ djc_helper.py
@@ -1803,7 +1803,7 @@ class DjcHelper:
             "774799", "774800",
         ]
         # note: 当前更新至（定期刷新这个值）
-        current_updated = 17
+        current_updated = 18
         for _idx, flowid in enumerate(watch_comic_flowids):
             idx = _idx + 1
             if idx > current_updated:

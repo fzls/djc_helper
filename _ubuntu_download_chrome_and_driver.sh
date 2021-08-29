3f3557885707164608ec08e36138885c770d20cc
风之凌殇 <fzls.zju@gmail.com>
2021-08-29 18:10:27 +0800

    安装指令默认yes

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install wget unzip
+sudo apt-get install -y wget unzip
 
 wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 wget -N https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip

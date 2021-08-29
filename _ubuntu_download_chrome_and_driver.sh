9a621b5044d99ca4772d747ce37628f95e8e178a
风之凌殇 <fzls.zju@gmail.com>
2021-08-30 02:06:11 +0800

    清除不必要的缓存

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt-get update && apt-get install -y wget unzip
+sudo apt-get update \
+    && apt-get install -y wget unzip \
+    && rm -rf /var/lib/apt/lists/*
 
 wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 wget -N https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
--- _ubuntu_install_chrome_and_driver.sh
+++ _ubuntu_install_chrome_and_driver.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 sudo dpkg -i google-chrome*.deb
-sudo apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -f -y
+sudo apt-get update \
+  && DEBIAN_FRONTEND=noninteractive apt-get install -f -y \
+  && rm -rf /var/lib/apt/lists/*
+
 sudo mv chromedriver /usr/local/bin/
 sudo chmod +x /usr/local/bin/chromedriver
 

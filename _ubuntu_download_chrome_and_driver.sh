508a4eac71a6a63693159e99dc4dc6b0a4bc5200
风之凌殇 <fzls.zju@gmail.com>
2021-08-29 18:07:59 +0800

    安装脚本增加安装wget

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install unzip
+sudo apt-get install wget unzip
 
 wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
 wget -N https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip

bfdd5d9e0b0ebc76a6351e1b9c7786a3414b70d9
风之凌殇 <fzls.zju@gmail.com>
2021-08-30 21:13:36 +0800

    封装获取腾讯云函数下可读写目录的函数，方便复用

--- const.py
+++ const.py
@@ -4,14 +4,20 @@ import os
 appVersion = 106
 guanjia_skey_version = 2
 
-# 特殊处理腾讯云函数
-base_dir_prefix = ""
-if os.getenv("TENCENTCLOUD_RUNENV") is not None:
-    base_dir_prefix = "/tmp/"
+tmp_path = "/tmp/"
+
+
+def get_final_dir_path(current_dir: str) -> str:
+    if os.getenv("TENCENTCLOUD_RUNENV") is not None and not current_dir.startswith(tmp_path):
+        # 腾讯云函数运行环境下仅/tmp目录可写
+        return tmp_path + current_dir
+
+    return current_dir
+
 
 # 定义一些目录
-db_top_dir = base_dir_prefix + ".db"
-cached_dir = base_dir_prefix + ".cached"
+db_top_dir = get_final_dir_path(".db")
+cached_dir = get_final_dir_path(".cached")
 
 downloads_dir = f"{cached_dir}/downloads"
 compressed_temp_dir = f"{cached_dir}/compressed"

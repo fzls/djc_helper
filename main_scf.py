a9f7717aa0abc70180dd9ee000a851e93d585edc
风之凌殇 <fzls.zju@gmail.com>
2021-08-30 20:15:53 +0800

    增加初版的云函数入口文件

---
+++ main_scf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 云函数入口文件（Serverless Cloud Function，SCF）
+# https://cloud.tencent.com/document/product/583/56051
+
+from flask import Flask, request
+
+from main import main_wrapper
+
+app = Flask(__name__)
+
+
+@app.route('/event-invoke', methods=['POST'])
+def invoke():
+    # Get all the HTTP headers from the official documentation of Tencent
+    request_id = request.headers.get("X-Scf-Request-Id", "")
+    print("SCF Invoke RequestId: " + request_id)
+
+    event = request.get_data()
+    event_str = event.decode("utf-8")
+
+    print(f"SCF event function  input: {event_str} request_id: {request_id}\n")
+
+    main_wrapper()
+
+    return f"{event_str} request_id: {request_id} completed"
+
+
+@app.route('/web-invoke/python-flask-http', methods=['POST', 'GET'])
+def web_invoke():
+    # Get all the HTTP headers from the official documentation of Tencent
+    request_id = request.headers.get("X-Scf-Request-Id", "")
+    print("SCF Invoke RequestId: " + request_id)
+
+    event = request.get_data()
+    event_str = event.decode("utf-8")
+
+    return "Hello from SCF HTTP function, your input: " + event_str + ", request_id: " + request_id + "\n"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9000)

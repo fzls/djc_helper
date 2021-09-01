798c14f4f1660611249a7c5c1f9af539cc5fdaed
风之凌殇 <fzls.zju@gmail.com>
2021-09-01 17:16:12 +0800

    wget改成curl，看看是否能解决github action偶尔出错的问题

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+apt-get install sudo
+
 sudo apt-get update \
-    && apt-get install -y wget unzip \
+    && apt-get install -y curl unzip \
     && rm -rf /var/lib/apt/lists/*
 
-wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
-wget -N https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+curl -O http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
+curl -O https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip

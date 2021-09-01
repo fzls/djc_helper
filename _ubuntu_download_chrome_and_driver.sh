cb00efc6a2c9cca81ef5d8a4a2cfb906f5e931a6
风之凌殇 <fzls.zju@gmail.com>
2021-09-01 17:26:25 +0800

    改回wget

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -3,9 +3,9 @@
 apt-get install sudo
 
 sudo apt-get update \
-    && apt-get install -y curl unzip \
+    && apt-get install -y wget unzip \
     && rm -rf /var/lib/apt/lists/*
 
-curl -LJO http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
-curl -LJO https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
+wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_92.0.4515.159-1_amd64.deb
+wget -N https://chromedriver.storage.googleapis.com/92.0.4515.107/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip

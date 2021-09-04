1b3e7ef9888c3575164069b7478740086d06922b
风之凌殇 <fzls.zju@gmail.com>
2021-09-04 18:51:00 +0800

    调整初始化脚本

--- _init_and_activate_venv.sh
+++ _init_and_activate_venv.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 python3 -m venv .venv
 source .venv/bin/activate
 
 python -m pip install -i https://pypi.doubanio.com/simple --upgrade pip setuptools wheel
-pip install -i https://pypi.doubanio.com/simple -r requirements.txt
+pip install -i https://pypi.doubanio.com/simple --upgrade -r requirements.txt

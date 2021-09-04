1e5547a7f3fd60efcad5a0f193dfe4a0bfee2efc
风之凌殇 <fzls.zju@gmail.com>
2021-09-04 18:39:32 +0800

    增加新的初始化并激活venv的脚本

---
+++ _init_and_activate_venv.sh
@@ -0,0 +1,5 @@
+python3 -m venv .venv
+source .venv/bin/activate
+
+python -m pip install -i https://pypi.doubanio.com/simple --upgrade pip setuptools wheel
+pip install -i https://pypi.doubanio.com/simple -r requirements.txt

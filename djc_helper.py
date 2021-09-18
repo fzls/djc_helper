260c16983e8b1f796b53dbb1c1a39de2d3f48a5b
风之凌殇 <fzls.zju@gmail.com>
2021-09-19 02:03:57 +0800

    移除多余的一个日志

--- djc_helper.py
+++ djc_helper.py
@@ -3681,9 +3681,6 @@ class DjcHelper:
         # 会员活动
         self.temporary_change_bind_and_do(f"从当前服务器选择一个公会会员角色参与公会会员活动（优先当前绑定角色）", self.query_dnf_rolelist_for_temporary_change_bind(), self.check_dnf_gonghui, guild_member_operations)
 
-        # re: r @2021-09-19 01:44:24 By Chen Ji
-        return
-
         # 兑换奖励
         def exchange_awards():
             awards = [

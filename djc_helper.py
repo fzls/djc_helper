b28f8e18cf6db4368894603603bc9ecfdf715de0
风之凌殇 <fzls.zju@gmail.com>
2021-10-13 16:08:04 +0800

    feat: colg活动快过期时使用不同的提示title，增加显示过期时间点

--- djc_helper.py
+++ djc_helper.py
@@ -5349,18 +5349,21 @@ class DjcHelper:
 
         if len(untaken_awards) > 0:
             need_show_message_box = False
+            title = ""
 
             # 如果有剩余奖励
             act_config = get_not_ams_act("colg每日签到")
             if act_config is not None and will_act_expired_in(act_config.dtEndTime, timedelta(days=5)):
                 # 活动即将过期时，则每天提示一次
                 need_show_message_box = is_daily_first_run(f"colg_{info.activity_id}2_领取奖励_活动即将结束时_每日提醒")
+                title = f"活动快过期了，记得领取奖励（过期时间为 {act_config.dtEndTime}）"
             else:
                 # 否则，每周提示一次
                 need_show_message_box = is_weekly_first_run(f"colg_{info.activity_id}2_领取奖励_每周提醒")
+                title = "可以领奖励啦"
 
             if need_show_message_box:
-                async_message_box(msg, "可以领奖励啦", open_url="https://bbs.colg.cn/forum-171-1.html", print_log=False)
+                async_message_box(msg, title, open_url="https://bbs.colg.cn/forum-171-1.html", print_log=False)
 
         logger.info(color("bold_cyan") + "除签到外的任务条件，以及各个奖励的领取，请自己前往colg进行嗷")
 

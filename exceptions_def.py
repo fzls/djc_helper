6ef1d8eca63b58ad58ce19138678046350602ae7
风之凌殇 <fzls.zju@gmail.com>
2021-11-01 16:45:35 +0800

    feat: dnf助手编年史token不对或者过期时，不再尝试后续的等级奖励领取

--- exceptions_def.py
+++ exceptions_def.py
@@ -12,3 +12,10 @@ class SameAccountTryLoginAtMultipleThreadsException(Exception):
     一般出现于在超快速模式下skey中途过期，导致后续运行时同一个账号在同时运行多个活动时，同时检测到skey过期而同时登录的问题
     """
     pass
+
+
+class DnfHelperChronicleTokenExpiredOrWrongException(Exception):
+    """
+    dnf助手编年史的token过期或者不对
+    """
+    pass

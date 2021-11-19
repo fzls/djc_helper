4b2e25ba03c86177ac3ce228d8c22b73cef3b70e
风之凌殇 <fzls.zju@gmail.com>
2021-11-19 14:10:43 +0800

    feat: 升级chrome版本到96.0.4664.45

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -9,6 +9,6 @@ sudo apt-get update \
 # 更新地址：
 #   https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable
 #   https://chromedriver.storage.googleapis.com/index.html
-wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_94.0.4606.81-1_amd64.deb
-wget -N https://chromedriver.storage.googleapis.com/94.0.4606.61/chromedriver_linux64.zip
+wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_96.0.4664.45-1_amd64.deb
+wget -N https://chromedriver.storage.googleapis.com/96.0.4664.45/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip

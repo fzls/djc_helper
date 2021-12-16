b0a633aa4d3e8f13a299f44036b8c6433f6f8ceb
风之凌殇 <fzls.zju@gmail.com>
2021-12-16 23:16:28 +0800

    feat: 集卡活动提示挪到小提示中

--- main_def.py
+++ main_def.py
@@ -1340,6 +1340,10 @@ def show_tips(cfg):
             "colg有个新的投票活动，可以自己去点一点领取-。-\n"
             "https://bbs.colg.cn/plugin.php?id=colg_activity_new:annual_ceremony&from=2021_annual_ceremony_offcial\n"
         ),
+        "1216-旧版集卡活动": (
+            "之前集卡18号结束，与新集卡有两天重合，如果需要继续做，请手动做或者使用之前版本运行~\n",
+            "https://act.qzone.qq.com//v2/vip/tx/p/7224_a45de826"
+        ),
     }
 
     logger.info(color("bold_green") + "如果看上去卡在这了，请看看任务是否有弹窗的图标，把他们一个个按掉就能继续了（活动此时已经运行完毕）")

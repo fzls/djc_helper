a8d5f4f338dcadc59dbbbad1c0c421596198e688
风之凌殇 <fzls.zju@gmail.com>
2021-12-20 00:37:09 +0800

    fix: lint

--- test_lanzou.py
+++ test_lanzou.py
@@ -5,10 +5,10 @@ from lanzou.api.utils import is_file_url, is_folder_url
 def test_is_file_url():
     # 确保每个备案的域名都能正确判定
     for domain in LanZouCloud().available_domains:
-        assert is_file_url(f'https://pan.{domain}/i1234abcd')
+        assert is_file_url(f"https://pan.{domain}/i1234abcd")
 
 
 def test_is_folder_url():
     # 确保每个备案的域名都能正确判定
     for domain in LanZouCloud().available_domains:
-        assert is_folder_url(f'https://pan.{domain}/b1234abcd')
+        assert is_folder_url(f"https://pan.{domain}/b1234abcd")

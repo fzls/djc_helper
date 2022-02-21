97e94566499fd34821988a1ea3392968bb177a2b
风之凌殇 <fzls.zju@gmail.com>
2022-02-22 00:34:05 +0800

    feat: 注释掉两个暂不可用的新域名

--- lanzou/api/core.py
+++ lanzou/api/core.py
@@ -69,8 +69,9 @@ class LanZouCloud(object):
         #   2. 把 最新的域名，比如 lanzouo 加到可能的域名列表的最前面
         #   3. 如果下面的域名有部分不再出现在备案信息中了，可以直接注释或者移除
         self.available_domains = [
-            'lanzouu.com',  # 2021-01-17 鲁ICP备15001327号-21
-            'lanzouh.com',  # 2021-01-17 鲁ICP备15001327号-20
+            # note: 下面两个似乎暂时不可用
+            # 'lanzouu.com',  # 2021-01-17 鲁ICP备15001327号-21
+            # 'lanzouh.com',  # 2021-01-17 鲁ICP备15001327号-20
             'lanzoum.com',  # 2021-01-17 鲁ICP备15001327号-19
             'lanzouf.com',  # 2021-01-17 鲁ICP备15001327号-18
             'lanzoub.com',  # 2021-01-17 鲁ICP备15001327号-17
@@ -87,7 +88,7 @@ class LanZouCloud(object):
             'lanzoui.com',  # 2020-06-09 鲁ICP备15001327号-6
             'lanzoux.com',  # 2020-06-09 鲁ICP备15001327号-5
         ]
-        
+
         # 默认链接使用最新的域名
         latest_domain = self.available_domains[0]
         self._host_url = re.sub(r'lanzou\w\.com', latest_domain, 'https://pan.lanzoup.com')

1ef8d4c51d11d06aa831f77d225db7992d3d3888
风之凌殇 <fzls.zju@gmail.com>
2022-04-05 17:23:18 +0800

    feat: 升级chrome版本到100.0.4896.75

--- _ubuntu_download_chrome_and_driver.sh
+++ _ubuntu_download_chrome_and_driver.sh
@@ -9,6 +9,6 @@ sudo apt-get update \
 # 更新地址：
 #   https://www.ubuntuupdates.org/package/google_chrome/stable/main/base/google-chrome-stable
 #   https://chromedriver.storage.googleapis.com/index.html
-wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_98.0.4758.80-1_amd64.deb
-wget -N https://chromedriver.storage.googleapis.com/98.0.4758.80/chromedriver_linux64.zip
+wget -N http://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_100.0.4896.75-1_amd64.deb
+wget -N https://chromedriver.storage.googleapis.com/100.0.4896.60/chromedriver_linux64.zip
 unzip -o chromedriver_linux64.zip

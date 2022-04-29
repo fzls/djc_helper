b981baf14eceb90e534f3491d4cbcd4a88fa6a02
风之凌殇 <fzls.zju@gmail.com>
2022-04-29 22:02:59 +0800

    fix: 修正蓝奏云解析 2022-03-05这种格式的时间时与其他时间的格式不一致的问题

--- test_lanzou.py
+++ test_lanzou.py
@@ -1,5 +1,8 @@
+import pytest
+
 from lanzou.api import LanZouCloud
-from lanzou.api.utils import is_file_url, is_folder_url
+from lanzou.api.utils import is_file_url, is_folder_url, time_format
+from util import parse_time
 
 
 def test_is_file_url():
@@ -12,3 +15,9 @@ def test_is_folder_url():
     # 确保每个备案的域名都能正确判定
     for domain in LanZouCloud().available_domains:
         assert is_folder_url(f"https://pan.{domain}/b1234abcd")
+
+
+def test_time_format():
+    # fix: 原先这里会直接返回 2022-03-25 ，与预期不符
+    time_str = time_format("2022-03-25")
+    parse_time(time_str, "%Y-%m-%d %H:%M:%S")

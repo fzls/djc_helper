0ade9d669ed7cdb3fba251a5e53632a1235baf25
风之凌殇 <fzls.zju@gmail.com>
2022-05-25 19:21:33 +0800

    feat: 新增 超级会员 活动

--- djc_helper.py
+++ djc_helper.py
@@ -574,7 +574,6 @@ class DjcHelper:
             ("翻牌活动", self.dnf_card_flip),
             ("dnf助手活动", self.dnf_helper),
             ("DNF集合站", self.dnf_collection),
-            ("超级会员", self.dnf_super_vip),
             ("黄钻", self.dnf_yellow_diamond),
             ("WeGame活动", self.dnf_wegame),
             ("qq视频蚊子腿-爱玩", self.qq_video_iwan),
@@ -583,6 +582,7 @@ class DjcHelper:
             ("冒险的起点", self.maoxian_start),
             ("DNF格斗大赛", self.dnf_pk),
             ("DNF马杰洛的规划", self.majieluo),
+            ("超级会员", self.dnf_super_vip),
         ]
 
     def expired_activities(self) -> list[tuple[str, Callable]]:
@@ -2113,7 +2113,7 @@ class DjcHelper:
         if self.lr is None:
             return
 
-        lucky_act_id = "41482_9ee9126d"
+        lucky_act_id = "66551_3dae8177"
         self.qzone_act_op("幸运勇士礼包 - 当前角色", lucky_act_id)
         self.qzone_act_op(
             "幸运勇士礼包 - 集卡幸运角色",
@@ -2122,20 +2122,20 @@ class DjcHelper:
                 "集卡", self.cfg.ark_lottery.lucky_dnf_server_id, self.cfg.ark_lottery.lucky_dnf_role_id
             ),
         )
-        self.qzone_act_op("勇士见面礼", "41483_7fd0d3bc")
+        self.qzone_act_op("勇士见面礼", "66552_521b4320")
         if not self.cfg.function_switches.disable_share and is_first_run(
             f"dnf_super_vip_{get_act_url('超级会员')}_分享_{self.uin()}"
         ):
             self.qzone_act_op(
                 "分享给自己",
-                "41484_69fb2a5c",
+                "66553_778d6b88",
                 act_req_data={
                     "receivers": [
                         self.qq(),
                     ]
                 },
             )
-        self.qzone_act_op("分享领取礼包", "41485_9c43b4c5")
+        self.qzone_act_op("分享领取礼包", "66554_ddb83bef")
 
     # --------------------------------------------QQ空间黄钻--------------------------------------------
     # note: 适配流程如下
@@ -10626,4 +10626,4 @@ if __name__ == "__main__":
         djcHelper.get_bind_role_list()
 
         # djcHelper.dnf_kol()
-        djcHelper.majieluo()
+        djcHelper.dnf_super_vip()

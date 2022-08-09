b9f8953e397502544733050d3362e7faebbea768
风之凌殇 <fzls.zju@gmail.com>
2022-08-09 14:20:10 +0800

    feat: 构建脚本移除打包相关流程，改为由github action完成

--- _release.py
+++ _release.py
@@ -1,6 +1,7 @@
 import json
 import os
 import re
+import webbrowser
 from datetime import datetime
 
 from _build import build
@@ -51,105 +52,14 @@ def release():
     release_7z_name = f"{release_dir_name}.7z"
     dir_github_action_artifact = "_github_action_artifact"
 
-    # ---------------构建
-    # 调用构建脚本
-    os.chdir(dir_src)
-    build()
-
-    # ---------------清除一些历史数据
-    make_sure_dir_exists(dir_all_release)
-    os.chdir(dir_all_release)
-    clear_github_artifact(dir_all_release, dir_github_action_artifact)
-
-    # ---------------打包
-    os.chdir(dir_src)
-    package(dir_src, dir_all_release, release_dir_name, release_7z_name, dir_github_action_artifact)
-
-    # ---------------构建增量补丁
-    create_patch_for_latest_n_version = 3
-
-    # ---------------构建增量包
-    os.chdir(dir_all_release)
-    show_head_line(f"开始构建增量包，最多包含过去{create_patch_for_latest_n_version}个版本到最新版本的补丁", color("bold_yellow"))
-    create_patch(dir_src, dir_all_release, create_patch_for_latest_n_version, dir_github_action_artifact)
-
-    # ---------------获取补丁地址（分开方便调试）
-    os.chdir(dir_all_release)
-    patch_file_name = create_patch(
-        dir_src,
-        dir_all_release,
-        create_patch_for_latest_n_version,
-        dir_github_action_artifact,
-        get_final_patch_path_only=True,
-    )
+    show_head_line("实际构建打包流程将在github action中自动执行，本地仅在git打tag", color("bold_yellow"))
 
     # ---------------标记新版本
     show_head_line("提交版本和版本变更说明，并同步到docs目录，用于生成github pages", color("bold_yellow"))
     os.chdir(dir_src)
     commit_new_version()
 
-    # ---------------上传到蓝奏云
-    show_head_line("开始上传到蓝奏云", color("bold_yellow"))
-    os.chdir(dir_src)
-    with open("upload_cookie.json") as fp:
-        cookie = json.load(fp)
-    os.chdir(dir_all_release)
-    uploader = Uploader()
-    uploader.login(cookie)
-    if uploader.login_ok:
-        logger.info("蓝奏云登录成功，开始上传压缩包")
-
-        def path_in_src(filepath_relative_to_src: str) -> str:
-            return os.path.realpath(os.path.join(dir_src, filepath_relative_to_src))
-
-        realpath = os.path.realpath
-
-        upload_info_list = [
-            (
-                uploader.folder_djc_helper,
-                [
-                    (realpath(release_7z_name), uploader.history_version_prefix),
-                    (path_in_src("utils/auto_updater.exe"), ""),
-                    (path_in_src("使用教程/使用文档.docx"), ""),
-                    (path_in_src("使用教程/视频教程.txt"), ""),
-                    (path_in_src("付费指引/付费指引.docx"), ""),
-                    (path_in_src("utils/不要下载增量更新文件_这个是给自动更新工具使用的.txt"), ""),
-                    (realpath(patch_file_name), uploader.history_patches_prefix),
-                ],
-            ),
-            (
-                uploader.folder_dnf_calc,
-                [
-                    (realpath(release_7z_name), uploader.history_version_prefix),
-                ],
-            ),
-        ]
-
-        logger.info(color("bold_green") + "具体上传列表如下：")
-        for upload_folder, upload_list in upload_info_list:
-            logger.info(color("bold_cyan") + f"\t{upload_folder.name}：")
-            for local_filepath, _history_file_prefix in upload_list:
-                logger.info(f"\t\t{local_filepath}")
-
-            logger.info("\n")
-
-        for upload_folder, upload_list in upload_info_list:
-            for local_filepath, history_file_prefix in reversed(upload_list):
-                # 逆序遍历，确保同一个网盘目录中，列在前面的最后才上传，从而在网盘显示时显示在最前方
-                total_try_count = 1
-                for try_index in range_from_one(total_try_count):
-                    upload_ok = uploader.upload_to_lanzouyun(
-                        local_filepath, upload_folder, history_file_prefix=history_file_prefix
-                    )
-                    if upload_ok:
-                        break
-
-                    logger.warning(f"第{try_index}/{total_try_count}次尝试上传{local_filepath}失败，等待一会后重试")
-                    if try_index < total_try_count:
-                        count_down("上传到网盘", 5 * try_index)
-
-    else:
-        logger.error("蓝奏云登录失败")
+    show_head_line("软件分发改用github release，不再尝试上传蓝奏云", color("bold_yellow"))
 
     # ---------------推送版本到github
     # 打包完成后git添加标签
@@ -157,9 +67,13 @@ def release():
     show_head_line("开始推送到github", color("bold_yellow"))
     push_github(version)
 
+    # ---------------查看github action
+    show_head_line("请在稍后打开的github action中查看打包结果", color("bold_yellow"))
+    webbrowser.open("https://github.com/fzls/djc_helper/actions/workflows/package.yml")
+
     # ---------------结束
     logger.info("+" * 40)
-    logger.info(color("bold_yellow") + f"{version} 发布完成，共用时{datetime.now() - run_start_time}，请检查上传至蓝奏云流程是否OK")
+    logger.info(color("bold_yellow") + f"{version} 发布完成，共用时{datetime.now() - run_start_time}，请等待github action的构建打包流程完成")
     logger.info("+" * 40)
 
     os.system("PAUSE")

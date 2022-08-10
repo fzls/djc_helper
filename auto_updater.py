a94f0ae465b24d990d524407941b4256f27195a4
风之凌殇 <fzls.zju@gmail.com>
2022-08-11 01:47:42 +0800

    feat: 调整dlc的日志颜色

--- auto_updater.py
+++ auto_updater.py
@@ -63,7 +63,7 @@ def auto_update():
 
     # 进行实际的检查是否需要更新操作
     latest_version = get_latest_version(uploader)
-    logger.info(f"当前版本为{args.version}，网盘最新版本为{latest_version}")
+    logger.info(color("bold_yellow") + f"当前版本为{args.version}，网盘最新版本为{latest_version}")
 
     if need_update(args.version, latest_version):
         update(args, uploader)

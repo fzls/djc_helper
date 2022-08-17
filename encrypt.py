0c71b4bebe1210969697c3d391a324cb2dbbd563
风之凌殇 <fzls.zju@gmail.com>
2022-08-17 23:01:13 +0800

    feat: 新增助手签名相关流程

---
+++ encrypt.py
@@ -0,0 +1,73 @@
+from __future__ import annotations
+
+import base64
+import hashlib
+import hmac
+from urllib.parse import quote_plus
+
+
+def make_dnf_helper_signature(
+    http_method: str,
+    api_path: str,
+    post_data: str,
+    secret: str,
+) -> str:
+    data = "&".join([
+        http_method,
+        quote_plus(api_path),
+        quote_plus(post_data)
+    ])
+
+    hash_bytes = hmac.new(secret.encode(), data.encode(), hashlib.sha1).digest()
+
+    signature = base64.b64encode(hash_bytes).decode()
+    return signature
+
+
+def make_dnf_helper_signature_data(data: dict[str, any]) -> str:
+    # 取出keys
+    keys = list(data.keys())
+
+    # 升序排序
+    keys.sort()
+
+    # 按顺序拼接成字符串
+    results = []
+    for key in keys:
+        results.append(f"{key}={data[key]}")
+
+    final_result = "&".join(results)
+
+    return final_result
+
+
+if __name__ == '__main__':
+    from urllib.parse import parse_qsl
+
+    query_string = "userId=504051073&gameId=1006&sPartition=11&sRoleId=71672841&game_code=dnf&token=wvtY7ern&uin=1054073896&uniqueRoleId=3482436497&openid=&appOpenid=4B92C052573D369D37CF9408B9112DA2&appidTask=1000042&cRand=1660746760049&tghappid=1000045"
+    query_data = dict(parse_qsl(query_string, keep_blank_values=True))
+
+    # query_data = {
+    #     "userId": "504051073",
+    #     "gameId": 1006,
+    #     "sPartition": "11",
+    #     "sRoleId": "71672841",
+    #     "game_code": "dnf",
+    #     "token": "wvtY7ern",
+    #     "uin": "1054073896",
+    #     "uniqueRoleId": "3482436497",
+    #     "openid": "",
+    #     "appOpenid": "4B92C052573D369D37CF9408B9112DA2",
+    #     "appidTask": 1000042,
+    #     "cRand": 1660746760049,
+    #     "tghappid": 1000045
+    # }
+
+    data = make_dnf_helper_signature_data(query_data)
+
+    print(make_dnf_helper_signature(
+        "GET",
+        "/peak/list/basic",
+        data,
+        "nKJH89hh@8yoHJ98y&IOhIUt9hbOh98ht",
+    ))
---
+++ test_encrypt.py
@@ -0,0 +1,36 @@
+from encrypt import make_dnf_helper_signature, make_dnf_helper_signature_data
+
+
+def test_make_dnf_helper_signature():
+    method = "POST"
+    api = "/yoyo/dnf/getuseractivitytopinfo"
+    post_data = "appOpenid=4B92C052573D369D37CF9408B9112DA2&cCurrentGameId=undefined&cGameId=1006&cRand=1660738585419&gameId=1006&game_code=dnf&openid=&sPartition=11&sRoleId=71672841&tghappid=1000045&token=wvtY7ern&uin=1054073896&uniqueRoleId=3482436497&userId=504051073"
+
+    secret = "nKJH89hh@8yoHJ98y&IOhIUt9hbOh98ht"
+
+    expect = "UM7eesly6VMgafca2rEVq657VdE="
+
+    assert make_dnf_helper_signature(method, api, post_data, secret) == expect
+
+
+def test_make_dnf_helper_signature_data():
+    data = {
+        "appOpenid": "4B92C052573D369D37CF9408B9112DA2",
+        "cCurrentGameId": "undefined",
+        "cGameId": "1006",
+        "cRand": "1660738585419",
+        "gameId": "1006",
+        "game_code": "dnf",
+        "openid": "",
+        "sPartition": "11",
+        "sRoleId": "71672841",
+        "tghappid": "1000045",
+        "token": "wvtY7ern",
+        "uin": "1054073896",
+        "uniqueRoleId": "3482436497",
+        "userId": "504051073"
+    }
+
+    expect = "appOpenid=4B92C052573D369D37CF9408B9112DA2&cCurrentGameId=undefined&cGameId=1006&cRand=1660738585419&gameId=1006&game_code=dnf&openid=&sPartition=11&sRoleId=71672841&tghappid=1000045&token=wvtY7ern&uin=1054073896&uniqueRoleId=3482436497&userId=504051073"
+
+    assert make_dnf_helper_signature_data(data) == expect

66b477665ed091a8f27114ba0bde70c597341b4d
风之凌殇 <fzls.zju@gmail.com>
2022-10-29 17:44:33 +0800

    feat: 上传小助手增加指定前缀

--- _upload_to_alist.py
+++ _upload_to_alist.py
@@ -8,20 +8,21 @@ from log import color, logger
 from version import now_version
 
 
-def upload_to_alist(files_to_upload: list[str]):
+def upload_to_alist(files_to_upload: list[tuple[str, str]]):
     logger.info(color("bold_yellow") + f"将上传下列文件到alist: {files_to_upload}")
 
-    for file_path in files_to_upload:
-        upload(file_path)
+    for file_path, old_version_name_prefix in files_to_upload:
+        upload(file_path, old_version_name_prefix=old_version_name_prefix)
 
 
 def main():
     dir_src = os.path.realpath(".")
     dir_all_release = os.path.realpath(os.path.join("releases"))
-    release_7z_path = os.path.join(dir_all_release, f"DNF蚊子腿小助手_v{now_version}_by风之凌殇.7z")
+    release_prefix = "DNF蚊子腿小助手_v"
+    release_7z_path = os.path.join(dir_all_release, f"{release_prefix}{now_version}_by风之凌殇.7z")
 
     upload_to_alist([
-        release_7z_path
+        (release_7z_path, release_prefix),
     ])
 
 

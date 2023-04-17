512dedd7e2330b420497962f711e017f4f9ce7ad
风之凌殇 <fzls.zju@gmail.com>
2023-04-17 22:36:11 +0800

    feat: 检查版本过久未更新的保底时间调整为45天，避免每年的几个固定一个多月没有活动的时候频繁报

--- update.py
+++ update.py
@@ -145,7 +145,8 @@ def get_latest_version_from_github(common_config: CommonConfig) -> str:
 @try_except()
 def notify_manual_check_update_on_release_too_long(config: CommonConfig):
     time_since_last_update = datetime.now() - datetime.strptime(ver_time, "%Y.%m.%d")
-    if time_since_last_update.days >= 21:
+    max_update_duration_days = 45
+    if time_since_last_update.days >= max_update_duration_days:
         msg = (
             f"当前版本更新于{ver_time}，距今已有{time_since_last_update}。\n"
             "可能原因如下:\n"

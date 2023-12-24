7d08512204a84ca83554e816350a03943eb1f6aa
风之凌殇 <fzls.zju@gmail.com>
2023-12-25 05:07:09 +0800

    feat: 判断下非空

--- config_ui.py
+++ config_ui.py
@@ -2585,7 +2585,7 @@ class AccountConfigUi(QWidget):
             if key in all_item_keys:
                 # 如果配置文件中的对应条目的名字与这里不一样，则改为这里的，这样偶尔填错了名字，也可以在后续通过代码内修正为正确的名字
                 item_cfg_from_config_file = cfg.get_xinyue_exchange_item_by_unique_key(key)
-                if item_cfg_from_config_file.sFlowName != item.sFlowName:
+                if item_cfg_from_config_file is not None and item_cfg_from_config_file.sFlowName != item.sFlowName:
                     item_cfg_from_config_file.sFlowName = item.sFlowName
 
                 continue

67449f3927472d4b0d4ca10ff9fa93ad7d9877d0
风之凌殇 <fzls.zju@gmail.com>
2024-06-28 04:17:22 +0800

    refactor: 将不再使用的旧版集卡相关代码挪到活动墓地里

--- djc_helper.py
+++ djc_helper.py
@@ -131,7 +131,7 @@ from network import Network, check_tencent_game_common_status_code, extract_qq_v
 from qq_login import LoginResult, QQLogin
 from qzone_activity import QzoneActivity
 from server import get_match_server_api
-from setting import dnf_server_id_to_area_info, dnf_server_id_to_name, parse_card_group_info_map, zzconfig
+from setting import dnf_server_id_to_area_info, dnf_server_id_to_name
 from sign import getACSRFTokenForAMS, getMillSecondsUnix
 from urls import (
     Urls,
@@ -209,8 +209,6 @@ class DjcHelper:
     def __init__(self, account_config, common_config, user_buy_info: BuyInfo | None = None):
         self.cfg: AccountConfig = account_config
         self.common_cfg: CommonConfig = common_config
-
-        self.zzconfig = zzconfig()
 
         # 初始化部分字段
         self.lr: LoginResult | None = None
@@ -2622,67 +2620,6 @@ class DjcHelper:
         except Exception as e:
             logger.exception("腾讯游戏信用这个经常挂掉<_<不过问题不大，反正每月只能领一次", exc_info=e)
 
-    # --------------------------------------------QQ空间集卡--------------------------------------------
-    @try_except()
-    def ark_lottery(self):
-        # note: 启用和废弃抽卡活动的流程如下
-        #   1. 启用
-        #   1.0 电脑chrome中设置Network conditions中的User agent为手机QQ的： Mozilla/5.0 (Linux; U; Android 5.0.2; zh-cn; X900 Build/CBXCNOP5500912251S) AppleWebKit/533.1 (KHTML, like Gecko)Version/4.0 MQQBrowser/5.4 TBS/025489 Mobile Safari/533.1 V1_AND_SQ_6.0.0_300_YYB_D QQ/6.0.0.2605 NetType/WIFI WebP/0.3.0 Pixel/1440
-        #   1.1 获取新配置   chrome设置为手机qq UA后，登录抽卡活动页面 get_act_url("集卡") ，然后打开主页源代码，从中搜索【window.syncData】找到逻辑数据和配置，将其值复制到【setting/ark_lottery.py】中，作为setting变量的值
-        #   1.2 填写新链接   在 urls.py 中，替换self.ark_lottery_page 的值为新版抽卡活动的链接（理论上应该只有 zz 和 verifyid 参数的值会变动，而且大概率是+1）
-        #   1.3 重新启用代码
-        #   1.3.1 在 djc_helper.py 中将 ark_lottery 的调用处从 expired_activities 移到 payed_activities
-        #   1.3.2 在 config.toml 和 config.example.toml 中 act_id_to_cost_all_cards_and_do_lottery 中增加新集卡活动的默认开关
-        #   1.4 更新 urls.py 中 not_ams_activities 中集卡活动的时间
-        #
-        # hack:
-        #   2. 废弃
-        #   2.1 在 djc_helper.py 中将 ark_lottery 的调用处从 normal_run 移到 expired_activities
-
-        # get_act_url("集卡")
-        show_head_line(f"QQ空间集卡 - {self.zzconfig.actid}_{self.zzconfig.actName}")
-        self.show_not_ams_act_info("集卡")
-
-        if not self.cfg.function_switches.get_ark_lottery:
-            logger.warning("未启用领取QQ空间集卡功能，将跳过")
-            return
-
-        self.fetch_pskey()
-        if self.lr is None:
-            return
-
-        qa = QzoneActivity(self, self.lr)
-        qa.ark_lottery()
-
-    def ark_lottery_query_left_times(self, to_qq):
-        ctx = f"查询 {to_qq} 的剩余被赠送次数"
-        res = self.get(
-            ctx, self.urls.ark_lottery_query_left_times, to_qq=to_qq, actName=self.zzconfig.actName, print_res=False
-        )
-        # # {"13320":{"data":{"uAccuPoint":4,"uPoint":3},"ret":0,"msg":"成功"},"ecode":0,"ts":1607934735801}
-        if res["13320"]["ret"] != 0:
-            return 0
-        return res["13320"]["data"]["uPoint"]
-
-    def send_card(self, card_name: str, cardId: str, to_qq: str, print_res=False) -> dict:
-        from_qq = self.qq()
-
-        ctx = f"{from_qq} 赠送卡片 {card_name}({cardId}) 给 {to_qq}"
-        return self.get(
-            ctx,
-            self.urls.ark_lottery_send_card,
-            cardId=cardId,
-            from_qq=from_qq,
-            to_qq=to_qq,
-            actName=self.zzconfig.actName,
-            print_res=print_res,
-        )
-        # # {"13333":{"data":{},"ret":0,"msg":"成功"},"ecode":0,"ts":1607934736057}
-
-    def send_card_by_name(self, card_name, to_qq):
-        card_info_map = parse_card_group_info_map(self.zzconfig)
-        return self.send_card(card_name, card_info_map[card_name].id, to_qq, print_res=True)
-
     def fetch_pskey(self, force=False, window_index=1):
         self.lr = None
 
@@ -3047,7 +2984,15 @@ class DjcHelper:
 
     # note: xxx. 修改 urls.py 中的 pesudo_ark_lottery_act_id ，将其加一即可
 
-    # re: 除此之外有一些额外的部分，参照旧版集卡 def ark_lottery(self): 的操作指引
+    # note: 启用和废弃抽卡活动的流程如下
+    #   1.1 在 djc_helper.py 中将 ark_lottery 的调用处从 expired_activities 移到 payed_activities
+    #   1.2 在 config.toml 和 config.example.toml 中 act_id_to_cost_all_cards_and_do_lottery 中增加新集卡活动的默认开关
+    #   1.3 更新 urls.py 中 not_ams_activities 中集卡活动的时间
+    #
+    # hack:
+    #   2. 废弃
+    #   2.1 在 djc_helper.py 中将 ark_lottery 的调用处从 normal_run 移到 expired_activities
+
     @try_except()
     def dnf_ark_lottery(self):
         get_act_url("集卡")

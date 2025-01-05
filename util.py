e01a20aba086794426347a100ce77bcade82e4b2
风之凌殇 <fzls.zju@gmail.com>
2025-01-05 20:33:47 +0800

    feat: 增加识别出错时错误信息包含unicode编码的情况，此时展示解码后的文本

--- util.py
+++ util.py
@@ -119,12 +119,18 @@ def check_some_exception(e: Exception, show_last_process_result=True) -> str:
 
         if last_response_info is not None:
             lr = last_response_info
-            msg += format_msg(
-                f"最近一次收到的请求结果为：status_code={lr.status_code} reason={lr.reason} \n{lr.text}\n", "bold_cyan"
-            )
 
-            text = parse_unicode_escape_string(lr.text)
-            logger.debug(f"最近一次收到的请求结果 文本after unicode-escape:\n{text}")
+            text = lr.text
+            unicode_decoded_text = parse_unicode_escape_string(lr.text)
+            if text != unicode_decoded_text:
+                text = unicode_decoded_text
+
+            msg += format_msg(
+                (
+                    f"最近一次收到的请求结果为：status_code={lr.status_code} reason={lr.reason}\n"
+                    f"{text}\n"
+                ), "bold_cyan"
+            )
 
     return msg
 

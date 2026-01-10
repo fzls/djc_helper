68158f9b96229cbb1dfadf4e3b38289321944b73
风之凌殇 <fzls.zju@gmail.com>
2026-01-10 15:11:23 +0800

    feat: skey校验活动改为绑定手机活动

--- djc_helper.py
+++ djc_helper.py
@@ -209,10 +209,10 @@ class DjcHelper:
         # res = self.dnf_comic_ide_op("判断skey是否过期", "248455", print_res=False)
         # res = self.dnf_bind_phone_op("判断skey是否过期", "971619", print_res=False)
         # res = self.dnf_cloud_game_op("判断skey是否过期", "1131647", print_res=False)
-        # res = self.dnf_bind_phone_ide_op("判断skey是否过期", "414203", print_res=False)
+        # res = self.dnf_luodiye_ide_op("判断skey是否过期", "485983", print_res=False)
 
-        # 预计结束时间: 2026-3-5
-        res = self.dnf_luodiye_ide_op("判断skey是否过期", "485983", print_res=False)
+        # 预计结束时间: 2029-3-31
+        res = self.dnf_bind_phone_ide_op("判断skey是否过期", "414203", print_res=False)
 
         if use_by_myself():
             msg = str(get_first_exists_dict_value(res, "msg", "sMsg") or "")
